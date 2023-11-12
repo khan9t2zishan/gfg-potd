568efb746346d4ac4fd6129c2169fac163d74ce0
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2023-11-12 14:17:35 +0530

    Check if string is rotated by two places
    Given two strings a and b. The task is to find if the string 'b' can be obtained by rotating (in any direction) string 'a' by exactly 2 places.
    
    Example 1:
    
    Input:
    a = amazon
    b = azonam
    Output: 
    1
    Explanation: 
    amazon can be rotated anti-clockwise by two places, which will make it as azonam.
    Example 2:
    
    Input:
    a = geeksforgeeks
    b = geeksgeeksfor
    Output: 
    0
    Explanation: 
    If we rotate geeksforgeeks by two place in any direction, we won't get geeksgeeksfor.
    Your Task:
    The task is to complete the function isRotated() which takes two strings as input parameters and checks if given strings can be formed by rotations. The function returns true if string 1 can be obtained by rotating string 2 by two places, else it returns false.
    
    Expected Time Complexity: O(N).
    Expected Auxilary Complexity: O(N).
    Challenge: Try doing it in O(1) space complexity.
    
    Constraints:
    1 ≤ length of a, b ≤ 10^5

---
+++ 2023/11 - November/12-11-2023.cpp
@@ -0,0 +1,53 @@
+/*
+
+Author : Zishan Khan 
+Date : 12/11/2023
+Problem : Check if string is rotated by two places
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/check-if-string-is-rotated-by-two-places-1587115620/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to check if a string can be obtained by rotating
+    //another string by exactly 2 places.
+    bool isRotated(string s1, string s2)
+    {
+       int n=s1.length();
+        if(n==2) return s1==s2;
+        for(int i=0;i<n;i++){
+            if( s1[(i+2)%n]==s2[i] || s1[(n+i-2)%n]==s2[i] ) continue;
+            else return false;
+        }
+        return true;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		string b;
+		cin>>s>>b;
+		Solution obj;
+		cout<<obj.isRotated(s,b)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
