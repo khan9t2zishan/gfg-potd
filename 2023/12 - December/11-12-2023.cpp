dafdec2275b03bca48b7b4aec951e9d033316b90
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2023-12-11 23:13:26 +0530

    Max Sum Subarray of size K
    Given an array of integers Arr of size N and a number K. Return the maximum sum of a subarray of size K.
    
    NOTE*: A subarray is a contiguous part of any given array.
    
    Example 1:
    
    Input:
    N = 4, K = 2
    Arr = [100, 200, 300, 400]
    Output:
    700
    Explanation:
    Arr3  + Arr4 =700,
    which is maximum.
    Example 2:
    
    Input:
    N = 4, K = 4
    Arr = [100, 200, 300, 400]
    Output:
    1000
    Explanation:
    Arr1 + Arr2 + Arr3 + Arr4 =1000,
    which is maximum.
    Your Task:
    
    You don't need to read input or print anything. Your task is to complete the function maximumSumSubarray() which takes the integer K, vector Arr with size N, containing the elements of the array and returns the maximum sum of a subarray of size K.
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 <= N <= 10^5
    1 <= Arri <= 10^5
    1 <= K <= N

---
+++ 2023/12 - December/11-12-2023.cpp
@@ -0,0 +1,54 @@
+/*
+
+Author : Zishan Khan 
+Date : 11/12/2023
+Problem : Max Sum Subarray of size K
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+
+*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+         long  ans = 0, sum = 0;
+        for(int i = 0; i < N; i++){
+            if(i < K) 
+                sum += Arr[i];
+            else{
+                ans = max(ans,sum);
+                sum += Arr[i];
+                sum -= Arr[i-K];
+            }
+        }
+        ans = max(ans,sum);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
