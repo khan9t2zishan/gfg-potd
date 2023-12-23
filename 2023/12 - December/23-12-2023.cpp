7e4729bf015fff4fad447835c2c5d3c7c6e51065
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2023-12-23 23:41:11 +0530

    Count More than n/k Occurences
    Given an array arr of size N and an element k. The task is to find the count of elements in the array that appear more than n/k times.
    
    Example 1:
    
    Input:
    N = 8
    arr = [3,1,2,2,1,2,3,3]
    k = 4
    Output: 
    2
    Explanation: 
    In the given array, 3 and 2 are the only elements that appears more than n/k times.
    Example 2:
    
    Input:
    N = 4
    arr = [2,3,3,2]
    k = 3
    Output: 
    2
    Explanation: In the given array, 3 and 2 are the only elements that appears more than n/k times. So the count of elements are 2.
    Your Task:
    The task is to complete the function countOccurence() which returns count of elements with more than n/k times appearance.
    
    Expected Time Complexity: O(N).
    Expected Auxiliary Space: O(N).
    
    Constraints:
    1 <= N <= 10^5
    1 <= a[i] <= 10^9
    1 <= k <= N

---
+++ 2023/12 - December/23-12-2023.cpp
@@ -0,0 +1,56 @@
+/*
+
+Author : Zishan Khan 
+Date : 23/12/2023
+Problem : Count More than n/k Occurences
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/count-element-occurences/1
+
+*/
+
+//{ Driver Code Starts
+// A C++ program to print elements with count more than n/k
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find all elements in array that appear more than n/k times.
+    int countOccurence(int arr[], int n, int k) {
+        
+       int ans=0;
+       unordered_map<int,int> mp;
+       for(int i=0;i<n;i++) mp[arr[i]]++;
+       for(auto it: mp){
+           if(it.second> n/k) ans++;
+       }
+       return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+
+        int arr[n];
+
+        for (i = 0; i < n; i++) cin >> arr[i];
+        int k;
+        cin >> k;
+        Solution obj;
+        cout << obj.countOccurence(arr, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
