7cf4d4e1f84cbc6887902732c3566ce716966ea7
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-01-01 21:43:39 +0530

    Array Pair Sum Divisibility Problem
    Given an array of integers nums and a number k, write a function that returns true if given array can be divided into pairs such that sum of every pair is divisible by k.
    
    Example 1 :
    
    Input : 
    nums = [9, 5, 7, 3]
    k = 6
    Output: 
    True
    Explanation: 
    {(9, 3), (5, 7)} is a 
    possible solution. 9 + 3 = 12 is divisible
    by 6 and 7 + 5 = 12 is also divisible by 6.
    Example 2:
    
    Input : 
    nums = [4, 4, 4]
    k = 4
    Output: 
    False
    Explanation: 
    You can make 1 pair at max, leaving a single 4 unpaired.
    Your Task:
    You don't need to read or print anything. Your task is to complete the function canPair() which takes array nums and k as input parameter and returns true if array can be divided into pairs such that sum of every pair is divisible by k otherwise returns false.
    
    Expected Time Complexity: O(n)
    Expected Space Complexity : O(n)
    
    Constraints:
    1 <= length( nums ) <= 10^5
    1 <= numsi <= 10^5
    1 <= k <= 10^5

---
+++ 2024/01- January/01-01-2024.cpp
@@ -0,0 +1,53 @@
+/*
+
+Author : Zishan Khan 
+Date : 01/01/2024
+Problem : Array Pair Sum Divisibility Problem
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/array-pair-sum-divisibility-problem3257/1
+
+*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+        if (nums.size() % 2 == 1) return false;
+
+        vector<int> arr(k, 0);
+        for (int i = 0; i < nums.size(); ++i) arr[nums[i] % k]++;
+        int i = 1, j = k - 1;
+        if (arr[0] % 2 == 1) return false;
+
+        while (i <= j) {
+            if (arr[i] != arr[j]) return false;
+            ++i;--j;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> nums(n);
+        for (int i = 0; i < nums.size(); i++) cin >> nums[i];
+        Solution ob;
+        bool ans = ob.canPair(nums, k);
+        if (ans)
+            cout << "True\n";
+        else
+            cout << "False\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
