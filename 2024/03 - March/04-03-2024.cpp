7dec5d20d1fef829053cbe3de0b4361294022acf
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-03-13 22:58:35 +0530

    Swap the array elements
    Given an array arr of n positive integers. The task is to swap every ith element of the array with (i+2)th element.
    
    Example 1:
    
    Input:
    n = 3
    arr[] = 1 2 3
    Output:
    3 2 1
    Explanation:
    Swapping 1 and 3, makes the array 3 2 1. There is only one swap possible in this array.
    Example 2:
    
    Input:
    n = 5
    arr[] = 1 2 3 4 5
    Output:
    3 4 5 2 1
    Explanation:
    Swapping 1 and 3, makes the array 3 2 1 4 5.
    Now, swapping 2 and 4, makes the array 3 4 1 2 5. 
    Again,swapping 1 and 5, makes the array 3 4 5 2 1.
    Your Task:
    Your task is to complete the function swapElements(), which should swap each ith element with (i+2)th element.
    
    Expected Time Complexity: O(n)
    Expected Auxilary Space: O(1)
    
    Constraints:
    1 <= n <= 10^6
    0 <= arri <= 10^9

--- 2024/03 - March/04-03-2024.cpp
+++ 2024/03 - March/04-03-2024.cpp
@@ -1,110 +1,65 @@
 /*
 
 Author : Zishan Khan
-Date : 13/03/2024
-Problem : Print matrix in diagonal pattern
-Difficulty : Easy
-Problem Link : https://www.geeksforgeeks.org/problems/print-matrix-in-diagonal-pattern/1
+Date : 04/03/2024
+Problem : Swap the array elements
+Difficulty : Easy 
+Problem Link : https://www.geeksforgeeks.org/problems/need-some-change/1
 
 */
 
-//{ Driver Code Starts
 
-#include <bits/stdc++.h>
+//{ Driver Code Starts
+//Initial Template for C++
+#include <iostream>
 using namespace std;
 
+
 // } Driver Code Ends
-/*You are required to complete this method */
+//User function Template for C++
 
-class Solution
-{
-public:
-    vector<int> matrixDiagonally(vector<vector<int>> &mat)
-    {
-        // Your code here
-        vector<int> ans;
-        int row = 0, col = 0;
-        int n = mat.size();
-
-        while (ans.size() < n * n)
-        {
-            // going upward diagonally till my first row or my last column is reached    //
-            while (row >= 0 && col < n)
-            {
-                if (row >= 0 && col >= 0 && row < n && col < n)
-                {
-                    ans.push_back(mat[row][col]);
-                    mat[row][col] = 1e9;
-                }
-                row--;
-                col++;
-            }
-            if (ans.size() == n * n)
-            {
-                break;
-            }
-
-            row = max(row, 0);
-            col = min(col, n - 1);
-
-            while (mat[row][col] == 1e9)
-            {
-                row++;
-            }
-
-            // going diagonally downwards
-            while (col >= 0 && row < n)
-            {
-                if (row >= 0 && col >= 0 && row < n && col < n)
-                {
-                    ans.push_back(mat[row][col]);
-                    mat[row][col] = 1e9;
-                }
-                row++;
-                col--;
-            }
-            if (ans.size() == n * n)
-            {
-                break;
-            }
-
-            row = min(row, n - 1);
-            col = max(col, 0);
-
-            while (mat[row][col] == 1e9)
-            {
-                col++;
-            }
-        }
-        return ans;
+/*Function to swap array elements
+* arr : array input
+* n : number of elements in array
+*/
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        for(int i = 0; i < n - 2; i++)
+            swap(arr[i], arr[i + 2]);
     }
 };
 
 //{ Driver Code Starts.
-int main()
-{
-    int T;
-    cin >> T;
 
-    while (T--)
-    {
-        int n;
-        int k = 0;
-        // cin>>k;
-        cin >> n;
-        vector<vector<int>> mat(n, vector<int>(n));
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                cin >> mat[i][j];
-            }
-        }
+// Driver code to test swapElements
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int n;
+	    cin >> n;
+	    
+	    int arr[n];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr[i];
+	    }
         Solution obj;
-        vector<int> ans = obj.matrixDiagonally(mat);
-        for (auto i : ans)
-            cout << i << " ";
-        cout << "\n";
-    }
+        // calling function to swap the array swap elements
+	    obj.swapElements(arr, n);
+	    
+	    // Printing the modified array
+	    for(int i = 0;i<n;i++){
+            cout << arr[i] << " ";
+        }
+	    
+	    cout << endl;
+	}
+	
+	return 0;
 }
 // } Driver Code Ends
