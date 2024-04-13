1a7b4c3e27c3b2be3659a75737e3edb92e60a093
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-04-13 21:56:57 +0530

    Reverse Bits
    Given a number x, reverse its binary form and return the answer in decimal.
    
    Example 1:
    
    Input:
    x = 1
    Output:
    2147483648 
    Explanation:
    Binary of 1 in 32 bits representation-
    00000000000000000000000000000001
    Reversing the binary form we get, 
    10000000000000000000000000000000,
    whose decimal value is 2147483648.
    Example 2:
    
    Input:
    x = 5
    Output:
    2684354560 
    Explanation:
    Binary of 5 in 32 bits representation-
    00000000000000000000000000000101
    Reversing the binary form we get, 
    10100000000000000000000000000000,
    whose decimal value is 2684354560.
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function reversedBits() which takes an Integer x as input and returns the reverse binary form of x in decimal form.
    
    Expected Time Complexity: O(log (x))
    Expected Auxiliary Space: O(1)
    
    Constraints:
    0  <=  x  <  2^32

---
+++ 2024/04 - April/13-04-2024.cpp
@@ -0,0 +1,44 @@
+/*
+
+Author : Zishan Khan 
+Date : 13/04/2024
+Problem : Reverse Bits
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/reverse-bits3556/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        long long out = 0;
+        for(int i = 0; i < 31; ++i)
+            out += x & 1,
+            out <<= 1,
+            x >>= 1;
+            
+        return out;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
