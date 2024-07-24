0e463a5aedbf0f50ec9160b56494b51e4b51400c
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-07-24 23:21:58 +0530

    Number of Rectangles in a Circle
    Given a circular sheet of radius, r. Find the total number of rectangles with integral length and width that can be cut from the sheet that can fit on the circle, one at a time.
    
    Examples :
    
    Input: r=1
    Output: 1
    Explanation: Only 1 rectangle of dimensions 1x1.
    Input: r=2
    Output: 8
    Explanation: The 8 possible rectangles are 
    (1x1)(1x2)(1x3)(2x1)(2x2)(2x3)(3x1)(3x2).
    Expected Time Complexity: O(r2)
    Expected Auxillary Space: O(1)
    
    
    Constraints:
    1<=r<=1000

--- 2024/06 - June/18-06-2024.cpp
+++ 2024/06 - June/18-06-2024.cpp
@@ -1,1 +1,50 @@
+/*
+
+Author : Zishan Khan 
+Date : 18/06/2024
+Problem : Number of Rectangles in a Circle
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/rectangles-in-a-circle0457/1
+
+*/
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int rectanglesInCircle(int R) {
+        int ans = 0;
+        int limit = 2 * R * 2 * R; 
+        for (int i = 1; i < 2 * R + 1; i++) {
+            for (int j = 1; j < 2 * R + 1; j++) {
+                if (i * i + j * j <= limit) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        int ans = ob.rectanglesInCircle(n);
+        cout << ans << "\n";
+    }
+}
+// } Driver Code Ends
 
