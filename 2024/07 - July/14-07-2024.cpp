69048dc58f0b59cd527d4be1e01831b7a6659319
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-08-04 21:19:00 +0530

    Segregate 0s and 1s
    Given an array arr consisting of only 0's and 1's in random order. Modify the array in-place to segregate 0s onto the left side and 1s onto the right side of the array.
    
    Examples :
    
    Input: arr[] = [0, 0, 1, 1, 0]
    Output: [0, 0, 0, 1, 1]
    Explanation:  After segregation, all the 0's are on the left and 1's are on the right. Modified array will be [0, 0, 0, 1, 1].
    Input: arr[] = [1, 1, 1, 1]
    Output: [1, 1, 1, 1]
    Explanation: There are no 0s in the given array, so the modified array is [1, 1, 1, 1]
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 ≤ arr.size() ≤ 10^6
    0 ≤ arr[i] ≤ 1

---
+++ 2024/07 - July/14-07-2024.cpp
@@ -0,0 +1,54 @@
+/*
+
+Author : Zishan Khan 
+Date : 14/07/2024
+Problem : Segregate 0s and 1s
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
+
+*/
+
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+       
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        ob.segregate0and1(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
