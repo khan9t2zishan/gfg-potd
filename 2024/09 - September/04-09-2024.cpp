ec2ed413ad90d7e04f0d9e67057cbca4533d777f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-04 22:36:02 +0530

    Count ways to N'th Stair(Order does not matter)
    There are n stairs, and a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top (order does not matter).
    Note: Order does not matter means for n = 4:- {1 2 1},{2 1 1},{1 1 2} are considered same.
    
    Examples :
    
    Input: n = 4
    Output: 3
    Explanation: Three ways to reach at 4th stair. They are {1, 1, 1, 1}, {1, 1, 2}, {2, 2}.
    Input: n = 5
    Output: 3
    Explanation: Three ways to reach at 5th stair. They are {1, 1, 1, 1, 1}, {1, 1, 2, 1} and {1, 2, 2}.

--- 2024/09 - September/04-09-2024.cpp
+++ 2024/09 - September/04-09-2024.cpp
@@ -1,1 +1,36 @@
+/*
 
+Author : Zishan Khan 
+Date : 04/09/2024
+Problem : Count ways to N'th Stair(Order does not matter)
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter5639/1
+
+*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		int nthStair(int n){
+		    return 1+n/2;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans  = ob.nthStair(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
