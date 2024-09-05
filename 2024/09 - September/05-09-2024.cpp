a25404f2d5bac8db1b192f0a4c2b1be498f6b9fb
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-05 21:12:51 +0530

    Missing in Array
    Given an array arr of size n−1 that contains distinct integers in the range of 1 to n (inclusive), find the missing element. The array is a permutation of size n with one element missing. Return the missing element.
    
    Examples:
    
    Input: n = 5, arr[] = [1,2,3,5]
    Output: 4
    Explanation : All the numbers from 1 to 5 are present except 4.
    Input: n = 2, arr[] = [1]
    Output: 2
    Explanation : All the numbers from 1 to 2 are present except 2.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 ≤ n ≤ 10^5
    1 ≤ arr[i] ≤ n

--- 2024/09 - September/05-09-2024.cpp
+++ 2024/09 - September/05-09-2024.cpp
@@ -1,1 +1,49 @@
+/*
 
+Author : Zishan Khan 
+Date : 05/09/2024
+Problem : Missing in Array
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1
+
+*/
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, vector<int>& arr) {
+
+       int sum=(n*(n+1))/2;
+        int sum1=accumulate(arr.begin(),arr.end(),0);
+        return sum-sum1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n - 1);
+        for (int i = 0; i < n - 1; ++i)
+            cin >> arr[i];
+        Solution obj;
+        cout << obj.missingNumber(n, arr) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
