de03ab115e24eed0fe54b9e30aee53cb70bca066
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-11 21:41:21 +0530

    Minimum Cost of ropes
    Given an array arr containing the lengths of the different ropes, we need to connect these ropes to form one rope. The cost to connect two ropes is equal to sum of their lengths. The task is to connect the ropes with minimum cost.  
    
    Examples:
    
    Input: arr[] = [4, 3, 2, 6]
    Output: 29
    Explanation: We can connect the ropes in following ways.
    1) First connect ropes of lengths 2 and 3. Which makes the array [4, 5, 6]. Cost of this operation 2 + 3 = 5. 
    2) Now connect ropes of lengths 4 and 5. Which makes the array [9, 6]. Cost of this operation 4 + 5 = 9.
    3) Finally connect the two ropes and all ropes have connected. Cost of this operation 9 + 6 =15
    Total cost for connecting all ropes is 5 + 9 + 15 = 29. This is the optimized cost for connecting ropes. 
    Other ways of connecting ropes would always have same or more cost. For example, if we connect 4 and 6 first (we get three rope of 3, 2 and 10), then connect 10 and 3 (we gettwo rope of 13 and 2). Finally we connect 13 and 2. Total cost in this way is 10 + 13 + 15 = 38.
    Input: arr[] = [4, 2, 7, 6, 9]
    Output: 62 
    Explanation: First, connect ropes 4 and 2, which makes the array [6, 7, 6, 9]. Cost of this operation 4 + 2 = 6. 
    Next, add ropes 6 and 6, which results in [12, 7, 9]. Cost of this operation 6 + 6 = 12.
    Then, add 7 and 9, which makes the array [12,16]. Cost of this operation 7 + 9 = 16. And
    finally, add these two which gives [28]. Hence, the total cost is 6 + 12 + 16 + 28 = 62.
    Expected Time Complexity: O(n logn)
    Expected Auxilliary Space: O(n)
    
    Constraints:
    1 ≤ arr.size() ≤ 20^5
    1 ≤ arr[i] ≤ 10^6

--- 2024/09 - September/11-09-2024.cpp
+++ 2024/09 - September/11-09-2024.cpp
@@ -1,1 +1,61 @@
+/*
 
+Author : Zishan Khan 
+Date : 11/09/2024
+Problem : Minimum Cost of ropes
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the minimum cost of connecting the ropes.
+    long long minCost(vector<long long>& arr) {
+        long long cost=0;
+        priority_queue<int,vector<int>,greater<int>>q;
+        for(int i=0;i<arr.size();i++){
+            q.push(arr[i]);
+        }
+        while(q.size()>1){
+            int a=q.top();
+            q.pop();
+            int b=q.top();
+            q.pop();
+            cost+=a+b;
+            q.push(a+b);
+        }
+        return cost;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        long long num;
+        vector<long long> a;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        cout << ob.minCost(a) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
