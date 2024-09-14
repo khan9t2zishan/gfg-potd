a8ce041483dbe5957bf2063729f9f9d5eccc9fae
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-14 18:50:50 +0530

    Alternate positive and negative numbers
    Given an unsorted array arr containing both positive and negative numbers. Your task is to create an array of alternate positive and negative numbers without changing the relative order of positive and negative numbers.
    Note: Array should start with a positive number and 0 (zero) should be considered a positive element.
    
    Examples:
    
    Input: arr[] = [9, 4, -2, -1, 5, 0, -5, -3, 2]
    Output: 9 -2 4 -1 5 -5 0 -3 2
    Explanation: Positive elements : [9,4,5,0,2]
    Negative elements : [-2,-1,-5,-3]
    As we need to maintain the relative order of postive elements and negative elements we will pick each element from the positive and negative and will store them. If any of the positive and negative numbersare completed. we will continue with the remaining signed elements.
    The output is 9,-2,4,-1,5,-5,0,-3,2.
    Input: arr[] = [-5, -2, 5, 2, 4, 7, 1, 8, 0, -8]
    Output: 5 -5 2 -2 4 -8 7 1 8 0
    Explanation : Positive elements : [5,2,4,7,1,8,0]
    Negative elements : [-5,-2,-8]
    The output is 5, -5, 2, -2, 4, -8, 7, 1, 8, 0.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(n)
    
    Constraints:
    1 ≤ arr.size() ≤ 10^7
    -10^6 ≤ arr[i] ≤ 10^7

--- 2024/09 - September/14-09-2024.cpp
+++ 2024/09 - September/14-09-2024.cpp
@@ -1,1 +1,65 @@
+/*
 
+Author : Zishan Khan 
+Date : 14/09/2024
+Problem : Alternate positive and negative numbers
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/array-of-alternate-ve-and-ve-nos1401/1
+
+*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    void rearrange(vector<int> &arr) {
+       vector<int> positive,negative;
+        for(auto x:arr){
+            if(x>=0)positive.push_back(x);
+            else negative.push_back(x);
+        }
+        int i=0,j=0,k=0;
+        while(i<positive.size() and j<negative.size()){
+            if(k%2)arr[k++]=negative[j++];
+            else arr[k++]=positive[i++];
+        }
+        while(i<positive.size())arr[k++]=positive[i++];
+        while(j<negative.size())arr[k++]=negative[j++];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        ob.rearrange(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
