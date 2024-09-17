a2d5bbd28b78c6c0ccb0c5b5af131d8d63bc0f1d
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-17 19:41:32 +0530

    Minimize the Heights II
    Given an array arr[] denoting heights of N towers and a positive integer K.
    
    For each tower, you must perform exactly one of the following operations exactly once.
    
    Increase the height of the tower by K
    Decrease the height of the tower by K
    Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
    
    You can find a slight modification of the problem here.
    Note: It is compulsory to increase or decrease the height by K for each tower. After the operation, the resultant array should not contain any negative integers.
    
    Examples :
    
    Input: k = 2, arr[] = {1, 5, 8, 10}
    Output: 5
    Explanation: The array can be modified as {1+k, 5-k, 8-k, 10-k} = {3, 3, 6, 8}.The difference between the largest and the smallest is 8-3 = 5.
    Input: k = 3, arr[] = {3, 9, 12, 16, 20}
    Output: 11
    Explanation: The array can be modified as {3+k, 9+k, 12-k, 16-k, 20-k} -> {6, 12, 9, 13, 17}.The difference between the largest and the smallest is 17-6 = 11. 
    Expected Time Complexity: O(n*logn)
    Expected Auxiliary Space: O(n)
    
    Constraints
    1 ≤ k ≤ 10^7
    1 ≤ n ≤ 10^5
    1 ≤ arr[i] ≤ 10^7

--- 2024/09 - September/17-09-2024.cpp
+++ 2024/09 - September/17-09-2024.cpp
@@ -1,1 +1,63 @@
+/*
+
+Author : Zishan Khan 
+Date : 17/09/2024
+Problem : Minimize the Heights II
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/minimize-the-heights3351/1
+
+*/
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+     int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        if(n==1)return 0;
+        sort(arr.begin(),arr.end());
+        int ans = arr[n-1]-arr[0], mn=0, mx=0;
+        for(int i=1;i<n;i++){
+            if(arr[i]-k<0)continue;
+            mn = min(arr[0]+k,arr[i]-k);
+            mx = max(arr[n-1]-k,arr[i-1]+k);
+            ans=min(ans,mx-mn);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin.ignore();
+        vector<int> a, b, c, d;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution ob;
+        auto ans = ob.getMinDiff(a, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
 
