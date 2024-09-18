abec48a1086e60872a34a4ef3d98a92b35deff2c
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-18 19:58:16 +0530

    Parenthesis Checker
    Given an expression string x. Examine whether the pairs and the orders of {,},(,),[,] are correct in exp.
    For example, the function should return 'true' for exp = [()]{}{[()()]()} and 'false' for exp = [(]).
    
    Note: The driver code prints "balanced" if function return true, otherwise it prints "not balanced".
    
    Examples :
    
    Input: {([])}
    Output: true
    Explanation: { ( [ ] ) }. Same colored brackets can form balanced pairs, with 0 number of unbalanced bracket.
    Input: ()
    Output: true
    Explanation: (). Same bracket can form balanced pairs,and here only 1 type of bracket is present and in balanced way.
    Input: ([]
    Output: false
    Explanation: ([]. Here square bracket is balanced but the small bracket is not balanced and Hence , the output will be unbalanced.
    Expected Time Complexity: O(|x|)
    Expected Auixilliary Space: O(|x|)
    
    Constraints:
    1 ≤ |x| ≤ 10^5

--- 2024/09 - September/18-09-2024.cpp
+++ 2024/09 - September/18-09-2024.cpp
@@ -1,1 +1,58 @@
+/*
 
+Author : Zishan Khan 
+Date : 18/09/2024
+Problem : Parenthesis Checker
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/parenthesis-checker2744/1
+
+*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+       stack<char>st;
+        for(auto i:x){
+            if(i=='('|| i=='[' || i=='{') st.push(i);
+            else{
+                 if(st.empty()) return false;
+                if(i==')' && st.top()=='(') st.pop();
+                else if(i==']' && st.top()=='[') st.pop();
+                else if(i=='}' && st.top()=='{') st.pop();
+                else return false;
+            }
+        }
+        if(!st.empty()) return false;
+        return true;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
