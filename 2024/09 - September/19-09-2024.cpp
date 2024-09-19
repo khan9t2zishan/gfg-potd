97e85c37c65af88be001a85c46e87a33630add80
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-19 21:42:02 +0530

    Reverse Words
    Given a String str, reverse the string without reversing its individual words. Words are separated by dots.
    
    Note: The last character has not been '.'. 
    
    Examples :
    
    Input: str = i.like.this.program.very.much
    Output: much.very.program.this.like.i
    Explanation: After reversing the whole string(not individual words), the input string becomes much.very.program.this.like.i
    Input: str = pqr.mno
    Output: mno.pqr
    Explanation: After reversing the whole string , the input string becomes mno.pqr
    Expected Time Complexity: O(|str|)
    Expected Auxiliary Space: O(|str|)
    
    Constraints:
    1 <= |str| <= 10^5

--- 2024/09 - September/19-09-2024.cpp
+++ 2024/09 - September/19-09-2024.cpp
@@ -1,1 +1,48 @@
+/*
 
+Author : Zishan Khan 
+Date : 19/09/2024
+Problem : Reverse Words
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1
+
+*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to reverse words in a given string.
+    string reverseWords(string str) {
+        // code here
+        int j=0;
+        for(int i=0;i<str.length();i++){
+            if(str[i]=='.'){
+                reverse(str.begin()+j,str.begin()+i);
+                j=i+1;
+            }
+            else if(i==str.length()-1)reverse(str.begin()+j,str.end());
+        }
+        reverse(str.begin(),str.end());
+        return str;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.reverseWords(s) << endl;
+    }
+}
+// } Driver Code Ends
