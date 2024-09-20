f034769377aa8929ac07001222290ec4b6a1b926
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-20 20:37:03 +0530

    Facing the sun
    Given an array height representing the heights of buildings. You have to count the buildings that will see the sunrise (Assume the sun rises on the side of the array starting point).
    Note: The height of the building should be strictly greater than the height of the buildings left in order to see the sun.
    
    
    Examples :
    
    Input: height = [7, 4, 8, 2, 9]
    Output: 3
    Explanation: As 7 is the first element, it can see the sunrise. 4 can't see the sunrise as 7 is hiding it. 8 can see. 2 can't see the sunrise. 9 also can see
    the sunrise.
    Input: height = [2, 3, 4, 5]
    Output: 4
    Explanation: As 2 is the first element, it can see the sunrise.  3 can see the sunrise as 2 is not hiding it. Same for 4 and 5, they also can see the sunrise.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)

--- 2024/09 - September/20-09-2024.cpp
+++ 2024/09 - September/20-09-2024.cpp
@@ -1,1 +1,56 @@
+/*
 
+Author : Zishan Khan 
+Date : 20/09/2024
+Problem : Facing the sun
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/facing-the-sun2126/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Returns count buildings that can see sunlight
+   int countBuildings(vector<int> &height) {
+        // code here
+        int ans = 1, mx = height[0];
+        for(auto x:height){
+            if(x>mx){
+                mx=x;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> height;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            height.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.countBuildings(height);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
