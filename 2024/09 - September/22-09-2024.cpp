d91749c2e9e05439c68ad8e6d490bd5030ba50fd
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-22 21:38:06 +0530

    Longest Prefix Suffix
    Given a string of characters, find the length of the longest proper prefix which is also a proper suffix.
    
    NOTE: Prefix and suffix can be overlapping but they should not be equal to the entire string.
    
    Examples :
    
    Input: str = "abab"
    Output: 2
    Explanation: "ab" is the longest proper prefix and suffix. 
    Input: str = "aaaa"
    Output: 3
    Explanation: "aaa" is the longest proper prefix and suffix. 
    Expected Time Complexity: O(|str|)
    Expected Auxiliary Space: O(|str|)
    
    Constraints:
    1 ≤ |str| ≤ 106
    str contains lower case English alphabets

--- 2024/09 - September/22-09-2024.cpp
+++ 2024/09 - September/22-09-2024.cpp
@@ -1,1 +1,61 @@
+/*
 
+Author : Zishan Khan 
+Date : 22/09/2024
+Problem : Longest Prefix Suffix
+Difficulty : Hard
+Problem Link : https://www.geeksforgeeks.org/problems/longest-prefix-suffix2527/1
+
+*/
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    int lps(string str) {
+        // Your code goes here
+        int i=1,j=0,n=str.length();
+        vector<int> storeLength(n,0);
+        while(i<n){
+            if(str[i]==str[j])storeLength[i++]=++j;
+            else{
+                if(j>0)j=storeLength[j-1];
+                else i++;
+            }
+        }
+        return storeLength[n-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+
+        Solution ob;
+
+        cout << ob.lps(str) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
