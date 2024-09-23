a495caf3e031bc5a47d25bcb48419b9afaa980c3
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-23 22:03:47 +0530

    Missing And Repeating
    Given an unsorted array arr of of positive integers. One number 'A' from set {1, 2,....,n} is missing and one number 'B' occurs twice in array. Find numbers A and B.
    
    Examples
    
    Input: arr[] = [2, 2]
    Output: 2 1
    Explanation: Repeating number is 2 and smallest positive missing number is 1.
    Input: arr[] = [1, 3, 3] 
    Output: 3 2
    Explanation: Repeating number is 3 and smallest positive missing number is 2.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    2 ≤ n ≤ 10^5
    1 ≤ arr[i] ≤ n

--- 2024/09 - September/23-09-2024.cpp
+++ 2024/09 - September/23-09-2024.cpp
@@ -1,1 +1,54 @@
+/*
 
+Author : Zishan Khan 
+Date : 23/09/2024
+Problem : Missing And Repeating
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/find-missing-and-repeating2512/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int element = abs(arr[i]);
+            if(arr[element-1]<0)ans.push_back(element);
+            else arr[element-1]*=-1;
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>0)ans.push_back(i+1);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
