2661af287d10418fb1ad3890992b94363d82706b
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-26 20:31:16 +0530

    Roof Top
    You are given the heights of consecutive buildings. You can move from the roof of a building to the roof of the next adjacent building. You need to find the maximum number of consecutive steps you can put forward such that you gain an increase in altitude with each step.
    
    Examples:
    
    Input: arr[] = [1, 2, 2, 3, 2]
    Output: 1
    Explanation: 1, 2, or 2, 3 are the only consecutive buildings with increasing heights thus maximum number of consecutive steps with an increase in gain in altitude would be 1 in both cases.
    Input: arr[] = [1, 2, 3, 4]
    Output: 3
    Explanation: 1 to 2 to 3 to 4 is the jump of length 3 to have a maximum number of buildings with increasing heights, so maximum steps with increasing altitude becomes 3.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 <= arr.size() <= 10^6
    1 <= arr[i] <= 10^5

--- 2024/09 - September/26-09-2024.cpp
+++ 2024/09 - September/26-09-2024.cpp
@@ -1,1 +1,57 @@
+/*
 
+Author : Zishan Khan 
+Date : 26/09/2024
+Problem : Roof Top
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/roof-top-1587115621/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+    int maxStep(vector<int>& arr) {
+        int consecutive_steps=0,maxi=0;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>arr[i-1]){
+                consecutive_steps++;
+                maxi=max(maxi,consecutive_steps);
+            }
+            else{
+                consecutive_steps=0;
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.maxStep(arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
