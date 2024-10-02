e64b6050ff9d257dbd8ebc02f11bce72e81fd554
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-02 18:01:23 +0530

    Rotate and delete
    Given an array arr integers. Assume sz to be the initial size of the array. Do the following operations exactly sz/2 times. In every kth (1<= k <= sz/2) operation:
    
    Right-rotate the array clockwise by 1.
    Delete the (n– k + 1)th element from begin.
    Now, Return the first element of the array.
    Note: Here n keeps on decreasing with every operation.
    
    Examples:
    
    Input: arr = [1, 2, 3, 4, 5, 6]
    Output: 3
    Explanation: Rotate the array clockwise i.e. after rotation the array arr = [6, 1, 2, 3, 4, 5] and delete the last element that is 5 that will be arr = [6, 1, 2, 3, 4]. Again rotate the array for the second time and deletes the second last element that is 2 that will be A = [4, 6, 1, 3], doing similar operation when we perform 4th operation, 4th last element does not exist. Then we deletes 1st element ie 1 that will be arr = [3, 6]. So, continuing this procedure the last element in arr is 2. So, the output will be 3.
    Input: arr = [1, 2, 3, 4]
    Output: 2
    Explanation: Rotate the vector clockwise i.e. after rotation the vector arr = [4, 1, 2, 3] and delete the last element that is 3 that will be arr = [4, 1, 2]. After doing all the operations, the output will be 2.
    Expected Time Complexity: O(n2)
    Expected Auxiliary Space: O(1)

--- 2024/10 - October/02-10-2024.cpp
+++ 2024/10 - October/02-10-2024.cpp
@@ -1,1 +1,61 @@
+/*
 
+Author : Zishan Khan 
+Date : 02/10/2024
+Problem : Rotate and delete
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/rotate-and-delete-1587115621/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int rotateDelete(vector<int> &arr) {
+        //CodeGenius
+        int n=arr.size();
+        int k=1;
+        while(n>1){
+            arr.insert(arr.begin(),arr.back());
+            arr.pop_back();
+            if(k>n) arr.erase(arr.begin());
+            else arr.erase(arr.end()-k);
+            n=arr.size();
+            k++;
+        }
+        return arr[0];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.rotateDelete(arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
