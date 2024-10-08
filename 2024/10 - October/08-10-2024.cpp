93b7f8bb23daa154bcf2777c2186ce48cc6cbdb7
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-08 06:49:19 +0530

    Largest Pair Sum
    Find the largest pair sum in an array of distinct integers.
    
    Examples :
    
    Input: arr[] = [12, 34, 10, 6, 40]
    Output: 74
    Explanation: Sum of 34 and 40 is the largest, i.e, 34 + 40 = 74.
    Input: arr[] = [10, 20, 30]
    Output: 50
    Explanation: 20 + 30 = 50.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    2 ≤ arr.size() ≤ 10^6
    0 ≤ arr[i] ≤ 10^6

--- 2024/10 - October/08-10-2024.cpp
+++ 2024/10 - October/08-10-2024.cpp
@@ -1,1 +1,62 @@
+/*
 
+Author : Zishan Khan 
+Date : 08/10/2024
+Problem : Largest Pair Sum
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/pair-sum--120604/1
+
+*/
+
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+         int pairsum(vector<int> &arr) {
+        int n=arr.size();
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(arr[i]>maxi){
+                maxi=arr[i];
+            }
+        }
+        int secmaxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(arr[i]>secmaxi&&arr[i]<maxi){
+                secmaxi=arr[i];
+            }
+        }
+        return maxi+secmaxi;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        int r = ob.pairsum(arr);
+        cout << r << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
