40c85d2aee8abc37ad450f246522b8bf1cd453e5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-11 19:30:01 +0530

    Reorganize The Array
    Given an array of elements arr[] with indices ranging from 0 to arr.size() - 1, your task is to write a program that rearranges the elements of the array such that arr[i] = i. If an element i is not present in the array, -1 should be placed at the corresponding index.
    
    Examples:
    
    Input: arr[] = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]
    Output: [-1, 1, 2, 3, 4, -1, 6, -1, -1, 9]
    Explanation: Here We can see there are 10 elements. So, the sorted array will look like [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] but in our array we are not having 0, 5, 7 and 8. So, at there places we will be printing -1 and otherplaces will be having elements.
    Input: arr[] = [2, 0, 1]
    Output: [0, 1, 2]
    Explanation: Here We can see all the elements are present so no -1 is returned in array.
    Expected Time Complexity: O(n).
    Expected Auxiliary Space: O(1).
    
    Constraints:
    0 ≤ arr.size() ≤ 10^5
    -1 ≤ arr[i] ≤ arr.size()-1

--- 2024/10 - October/11-10-2024.cpp
+++ 2024/10 - October/11-10-2024.cpp
@@ -1,1 +1,58 @@
+/*
 
+Author : Zishan Khan 
+Date : 11/10/2024
+Problem : Reorganize The Array
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/reorganize-the-array4810/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+   vector<int> rearrange( vector<int>& arr) {
+        
+        for(int i=0;i<arr.size();i++){
+            while(arr[i]!=-1 && arr[i]!=i){
+                swap(arr[i],arr[arr[i]]);
+            }
+        }
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution solution;
+        vector<int> ans = solution.rearrange(arr);
+
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
