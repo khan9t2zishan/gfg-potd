2c6cee062ff71b68208cc26e7698bffb559c80fd
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-16 16:46:30 +0530

    Two Swaps
    Given a permutation of some of the first natural numbers in an array arr[], determine if the array can be sorted in exactly two swaps. A swap can involve the same pair of indices twice.
    
    Return true if it is possible to sort the array with exactly two swaps, otherwise return false.
    
    Examples:
    
    Input: arr = [4, 3, 2, 1]
    Output: true
    Explanation: First, swap arr[0] and arr[3]. The array becomes [1, 3, 2, 4]. Then, swap arr[1] and arr[2]. The array becomes [1, 2, 3, 4], which is sorted.
    Input: arr = [4, 3, 1, 2]
    Output: false
    Explanation: It is not possible to sort the array with exactly two swaps.
    Constraints:
    1 ≤ arr.size() ≤ 10^6
    1 ≤ arr[i] ≤ arr.size()

--- 2024/10 - October/16-10-2024.cpp
+++ 2024/10 - October/16-10-2024.cpp
@@ -1,1 +1,60 @@
+/*
 
+Author : Zishan Khan 
+Date : 16/10/2024
+Problem : Two Swaps
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/two-swaps--155623/1
+
+*/
+
+
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=i+1){
+                swap(arr[i],arr[arr[i]-1]);
+                count++;
+                i--;
+            }
+            if(count>2)return false;
+        }
+        return count==2|count==0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        vector<int> arr;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution ob;
+        bool ans = ob.checkSorted(arr);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+}
+
+// } Driver Code Ends
