ae4bfffd10189d6a535f98c7ac137b85a6c0a012
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-18 21:13:53 +0530

    Single Number
    Given an array arr[] of positive integers where every element appears even times except for one. Find that number occurring an odd number of times.
    
    Examples:
    
    Input: arr[] = [1, 1, 2, 2, 2]
    Output: 2
    Explanation: In the given array all element appear two times except 2 which appears thrice.
    Input: arr[] = [8, 8, 7, 7, 6, 6, 1]
    Output: 1
    Explanation: In the given array all element appear two times except 1 which appears once.
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 ≤ arr.size() ≤ 10^6
    0 ≤ arri ≤ 10^5

--- 2024/10 - October/18-10-2024.cpp
+++ 2024/10 - October/18-10-2024.cpp
@@ -1,1 +1,53 @@
+/*
+
+Author : Zishan Khan 
+Date : 18/10/2024
+Problem : Single Number
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/single-number1014/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int getSingle(vector<int>& arr) {
+       int number=0;
+        for(int i=0;i<arr.size();i++){
+            number=number^arr[i];
+        }
+        return number;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.getSingle(arr);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
 
