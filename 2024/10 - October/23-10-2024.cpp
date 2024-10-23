0d82ab41c4a8e5f571c6d009932bb752dd25f556
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-23 17:44:22 +0530

    Find the Sum of Last N nodes of the Linked List
    Given a single linked list, calculate the sum of the last n nodes.
    
    Note: It is guaranteed that n <= number of nodes.
    
    Examples:
    
    Input: Linked List: 5->9->6->3->4->10, n = 3
    
    Output: 17
    Explanation: The sum of the last three nodes in the linked list is 3 + 4 + 10 = 17.
    Input: Linked List: 1->2, n = 2
    
    Output: 3
    Explanation: The sum of the last two nodes in the linked list is 2 + 1 = 3.
    Constraints:
    1 <= number of nodes, n <= 10^5
    1 <= node->data <= 10^3

--- 2024/10 - October/23-10-2024.cpp
+++ 2024/10 - October/23-10-2024.cpp
@@ -1,1 +1,94 @@
+/*
 
+Author : Zishan Khan 
+Date : 23/10/2024
+Problem : Find the Sum of Last N nodes of the Linked List
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/find-the-sum-of-last-n-nodes-of-the-linked-list/1
+
+*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    /*Structure of the node of the linled list is as
+
+    struct Node {
+        int data;
+        struct Node* next;
+
+        Node(int x){
+            data = x;
+            next = NULL;
+        }
+    };
+    */
+    // your task is to complete this function
+    // function should return sum of last n nodes
+    int sumOfLastN_Nodes(struct Node* head, int n) {
+        // Code here
+         if(!head|| n<=0) return 0;
+        Node *fast=head, *slow=head;
+        
+        for(int i=0;i<n;i++){
+            fast=fast->next;
+        }
+        while(fast){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        int sum=0;
+        while(slow){
+            sum=sum+slow->data;
+            slow=slow->next;
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n;
+        cin >> n;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        cout << ob.sumOfLastN_Nodes(head, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
