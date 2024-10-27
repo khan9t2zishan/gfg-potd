9183dd0a52eab6e12aeb55d78771670f98714836
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-27 18:10:11 +0530

    Triplet Family
    Given an array arr of integers. Find whether three numbers are such that the sum of two elements equals the third element.
    
    Example:
    
    Input: arr[] = [1, 2, 3, 4, 5]
    Output: true
    Explanation: The pair (1, 2) sums to 3.
    Input: arr[] = [5, 3, 4]
    Output: false
    Explanation: No triplets satisfy the condition.
    Expected Time Complexity: O(n2)
    Expected Auxilary Space: O(1)
    
    Constraints:
    1 <= arr.size() <= 10^3
    0 <= arr[i] <= 10^5

--- 2024/10 - October/27-10-2024.cpp
+++ 2024/10 - October/27-10-2024.cpp
@@ -1,1 +1,63 @@
+/*
 
+Author : Zishan Khan 
+Date : 27/10/2024
+Problem : Triplet Family
+Difficulty : Basic
+Problem Link : https://www.geeksforgeeks.org/problems/triplet-family/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool findTriplet(vector<int>& arr) {
+        // Your code
+         sort(arr.begin(),arr.end(),greater<int>());
+        for(int i=0;i<arr.size();i++){
+            int target=arr[i];
+            int start=i+1;
+            int end=arr.size()-1;
+            while(start<end){
+                if(arr[start]+arr[end]==target) return true;
+                else if(arr[start]+arr[end]>target) start++;
+                else end--;
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        bool res = obj.findTriplet(arr);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
