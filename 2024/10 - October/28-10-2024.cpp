921450076b8326a2738fc156527a37450d75fa41
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-28 21:44:48 +0530

    Remove duplicates in array
    Given an array arr consisting of positive integer numbers, remove all duplicate numbers.
    
    Example:
    
    Input: arr[] = [2, 2, 3, 3, 7, 5] 
    Output: [2, 3, 7, 5]
    Explanation: After removing the duplicates 2 and 3 we get 2 3 7 5.
    Input: arr[] = [2, 2, 5, 5, 7, 7] 
    Output: [2, 5, 7]
    Input: arr[] = [8, 7] 
    Output: [8, 7]
    Constraints:
    1<= arr.size() <=10^6
    2<= arr[i] <=100

--- 2024/10 - October/28-10-2024.cpp
+++ 2024/10 - October/28-10-2024.cpp
@@ -7,3 +7,53 @@ Difficulty : Easy
 Problem Link : https://www.geeksforgeeks.org/problems/remove-duplicates-in-small-prime-array/1
 
 */
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        // code here
+         int mm[101]={0};
+        vector<int> ans;
+        for(int x:arr){
+            if(!mm[x]){
+                ans.push_back(x);
+                mm[x]=1;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.removeDuplicate(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
