77b5d2ddf510c617df5c4c1cd5d9a6812896fafb
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-28 09:23:27 +0530

    Update 28-10-2024.cpp

--- 2024/10 - October/28-10-2024.cpp
+++ 2024/10 - October/28-10-2024.cpp
@@ -1,1 +1,9 @@
+/*
 
+Author : Zishan Khan 
+Date : 28/10/2024
+Problem : Remove duplicates in array
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/remove-duplicates-in-small-prime-array/1
+
+*/
