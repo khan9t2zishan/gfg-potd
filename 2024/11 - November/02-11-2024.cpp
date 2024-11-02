41f63e866d86ad9ba874d778ee35b08559971e58
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-11-02 17:02:52 +0530

    Kth distance
    Given an unsorted array arr and a number k which is smaller than the size of the array. Return true if the array contains any duplicate within k distance throughout the array else false.
    
    Examples:
    
    Input: arr[] = [1, 2, 3, 4, 1, 2, 3, 4] and k = 3
    Output: false
    Explanation: All duplicates are more than k distance away.
    Input: arr[] = [1, 2, 3, 1, 4, 5] and k = 3
    Output: true
    Explanation: 1 is repeated at distance 3.
    Input: arr[] = [6, 8, 4, 1, 8, 5, 7] and k = 3
    Output: true
    Explanation: 8 is repeated at distance 3.
    Constraints:
    1 ≤ arr.size() ≤ 10^6
    1 ≤ k < arr.size()
    1 ≤ arr[i] ≤ 10^5

--- 2024/11 - November/02-11-2024.cpp
+++ 2024/11 - November/02-11-2024.cpp
@@ -2,9 +2,64 @@
 
 Author : Zishan Khan 
 Date : 02/11/2024
-Problem : Copy Set Bits in Range
+Problem : Kth distance
 Difficulty : Easy
-Problem Link : https://practice.geeksforgeeks.org/problems/copy-set-bits-in-range0623/1
+Problem Link : https://www.geeksforgeeks.org/problems/kth-distance3757/1
 
 */
 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+
+    bool checkDuplicatesWithinK(vector<int>& arr, int k) {
+        // your code
+         unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            if(mp.find(arr[i])!=mp.end()){
+                if(i-mp[arr[i]]<=k) return true;
+                else mp[arr[i]]=i;
+            }
+            mp[arr[i]]=i;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        bool res = obj.checkDuplicatesWithinK(arr, k);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
