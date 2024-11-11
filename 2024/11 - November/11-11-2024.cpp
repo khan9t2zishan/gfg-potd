6c694f67c70fb10ad9f9a775627e71a2aadee85b
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-11-11 20:23:29 +0530

    Make array elements unique
    Given an array arr[ ], your task is to find the minimum number of increment operations required to make all the elements of the array unique. i.e.- no value in the array should occur more than once. In one operation, a value can be incremented by 1 only.
    
    Examples :
    
    Input: arr[] = [1, 2, 2]
    Output: 1
    Explanation: If we increase arr[2] by 1 then the resulting array becomes {1, 2, 3} and has all unique values.Hence, the answer is 1 in this case.
    Input: arr[] = [1, 1, 2, 3]
    Output: 3
    Explanation: If we increase arr[0] by 3, then all array elements will be unique. Hence, the answer is 3 in this case.
    Input: arr[] = [5, 4, 3, 2, 1]
    Output: 0
    Explanation: All elements are unique.
    Constraints:
    1 ≤ arr.size() ≤ 10^6
    0 ≤ arr[i] ≤ 10^6

---
+++ 2024/11 - November/11-11-2024.cpp
@@ -0,0 +1,11 @@
+/*
+
+Author : Zishan Khan 
+Date : 11/11/2024
+Problem : Make array elements unique
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/make-array-elements-unique--170645/1
+
+*/
+
+https://www.geeksforgeeks.org/problems/make-array-elements-unique--170645/1
