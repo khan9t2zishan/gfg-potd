eccc886eced35acf06fc2bd6bf1816659bf87ad5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-11-17 19:17:01 +0530

    Reverse an Array
    You are given an array of integers arr[]. Your task is to reverse the given array.
    
    Examples:
    
    Input: arr = [1, 4, 3, 2, 6, 5]
    Output: [5, 6, 2, 3, 4, 1]
    Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.
    Input: arr = [4, 5, 2]
    Output: [2, 5, 4]
    Explanation: The elements of the array are 4 5 2. The reversed array will be 2 5 4.
    Input: arr = [1]
    Output: [1]
    Explanation: The array has only single element, hence the reversed array is same as the original.
    Constraints:
    1<=arr.size()<=10^5
    0<=arr[i]<=10^5

--- 2024/11 - November/17-11-2024.cpp
+++ 2024/11 - November/17-11-2024.cpp
@@ -1,1 +1,56 @@
+/*
 
+Author : Zishan Khan 
+Date : 17/11/2024
+Problem : Reverse an Array
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/reverse-an-array/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+         int s=0;
+        int e=arr.size()-1;
+        while(s<e){
+            swap(arr[s],arr[e]);
+            s++;
+            e--;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
