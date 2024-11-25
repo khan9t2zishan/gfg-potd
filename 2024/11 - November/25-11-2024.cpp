398e75696b6c0cf5714c6e57a9282a5dc2e58044
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-11-25 23:07:28 +0530

    Update 25-11-2024.cpp

--- 2024/11 - November/25-11-2024.cpp
+++ 2024/11 - November/25-11-2024.cpp
@@ -1,1 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        // Your Code Here
+    int ans=INT_MIN,productStart=1,productEnd=1,n=arr.size();
+        for(int i=0;i<n;i++){
+            productStart*=arr[i];
+            productEnd*=arr[n-1-i];
+            ans=max({ans,productStart,productEnd});
+            if(productStart==0)productStart=1;
+            if(productEnd==0)productEnd=1;
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
