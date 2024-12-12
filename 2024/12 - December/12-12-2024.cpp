2c171fa0ceb7879309483e95c44c34da5c40f52e
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-12-12 22:52:41 +0530

    Update 12-12-2024.cpp

--- 2024/12 - December/12-12-2024.cpp
+++ 2024/12 - December/12-12-2024.cpp
@@ -1,1 +1,83 @@
+//{ Driver Code Starts
+// Initial function template for C++
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    
+       int firstOcc(vector<int>& arr, int target){
+        int s=0,e=arr.size()-1;
+        int ans=-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                e=mid-1;
+            }
+            else if(arr[mid]>target) e=mid-1;
+            else s=mid+1;
+        }
+        return ans;
+    }
+    int lastOcc(vector<int>& arr, int target){
+        int s=0,e=arr.size()-1;
+        int ans=-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                s=mid+1;
+            }
+            else if(arr[mid]>target) e=mid-1;
+            else s=mid+1;
+        }
+        return ans;
+    }
+    int countFreq(vector<int>& arr, int target) {
+        //Code
+        if(lastOcc(arr,target)==-1 && firstOcc(arr,target)==-1) return 0;
+        return lastOcc(arr,target)-firstOcc(arr,target) +1;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.countFreq(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
