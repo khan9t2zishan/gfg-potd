97a075b3c1f506f78c048e409d2b0e06e7fd18a2
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-12-13 20:06:46 +0530

    Update 13-12-2024.cpp

--- 2024/12 - December/13-12-2024.cpp
+++ 2024/12 - December/13-12-2024.cpp
@@ -1,1 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findMin(vector<int>& arr) {
+        // complete the function here
+    int n=arr.size(),low=0,high=n-1,mid=0;
+        while(low<high){
+            mid=(low+high)/2;
+            if(arr[mid]<=arr[high])high=mid;
+            else low=mid+1;
+        }
+        return arr[low];
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    Solution ob;
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        cout << ob.findMin(nums) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
