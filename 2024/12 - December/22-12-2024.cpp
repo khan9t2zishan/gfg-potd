5fc481a10d130ac8acdf44d77e653a6323559357
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-12-22 20:34:23 +0530

    Update 22-12-2024.cpp

--- 2024/12 - December/22-12-2024.cpp
+++ 2024/12 - December/22-12-2024.cpp
@@ -1,1 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        // your code here
+        int i=0,j=mat[0].size()-1,n=mat.size();
+        while(i<n and j>=0){
+            int temp = mat[i][j];
+            if(temp==x)return true;
+            else if(temp<x)i++;
+            else j--;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        int x;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        cin >> x;
+        Solution ob;
+        bool an = ob.matSearch(matrix, x);
+        if (an)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
