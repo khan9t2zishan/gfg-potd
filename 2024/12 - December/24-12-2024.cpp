b3b5099efa1c4f654cfa1e22b1ab4c9c7d1a2f96
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-12-24 22:04:40 +0530

    Update 24-12-2024.cpp

--- 2024/12 - December/24-12-2024.cpp
+++ 2024/12 - December/24-12-2024.cpp
@@ -1,1 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int n=mat.size(),m=mat[0].size(),low=0,high=n*m-1,mid=0;
+        while(low<=high){
+            mid=(low+high)/2;
+            int temp=mat[mid/m][mid%m];
+            if(temp==x)return true;
+            else if(temp<x)low=mid+1;
+            else high=mid-1;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(m, 0);
+            for (int j = 0; j < m; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        int x;
+        cin >> x;
+        Solution obj;
+        if (obj.searchMatrix(matrix, x))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
