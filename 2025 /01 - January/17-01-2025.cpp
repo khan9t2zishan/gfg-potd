4add8b1cf98593b950bca67f81ac1fb6eadb577d
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-01-17 21:51:36 +0530

    Update 17-01-2025.cpp

--- 2025 /01 - January/17-01-2025.cpp
+++ 2025 /01 - January/17-01-2025.cpp
@@ -1,1 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        vector<int> res(n);
+
+        int product = 1;
+        int zeroCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                zeroCount++;
+            } else {
+                product *= arr[i];
+            }
+        }
+
+        if (zeroCount > 1) {
+            return vector<int>(n, 0); // All zeros
+        }
+
+
+        if (zeroCount == 1) {
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == 0) {
+                    res[i] = product;
+                } else {
+                    res[i] = 0;
+                }
+            }
+            return res;
+        }
+
+        for (int i = 0; i < n; i++) {
+            res[i] = product / arr[i];
+        }
+
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution obj;
+        vector<int> res = obj.productExceptSelf(arr);
+
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
