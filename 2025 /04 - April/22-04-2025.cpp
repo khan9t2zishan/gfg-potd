8cc865f8d337abe2e00a7bb697b8c7c85c8c8e8b
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-04-22 20:52:12 +0530

    Update 22-04-2025.cpp

--- 2025 /04 - April/22-04-2025.cpp
+++ 2025 /04 - April/22-04-2025.cpp
@@ -1,1 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findUnique(vector<int> &arr) {
+        // code here
+         return accumulate(begin(arr),end(arr),0,bit_xor<>());
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.findUnique(arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
