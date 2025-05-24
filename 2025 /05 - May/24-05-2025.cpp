200f91b9245b6847c33017a57856d8200535bc0b
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-05-24 21:16:15 +0530

    Update 24-05-2025.cpp

--- 2025 /05 - May/24-05-2025.cpp
+++ 2025 /05 - May/24-05-2025.cpp
@@ -3,7 +3,7 @@ class Solution {
     int sumSubstrings(string &s) {
         // code here
          int n=s.length(), ans=0;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++){ 
             for(int j=i; j<n; j++){
                 ans+=(stoi(s.substr(i, j-i+1)));
             }
