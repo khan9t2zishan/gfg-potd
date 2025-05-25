b793bf6d2ccd888b7c4233842f75412432bbe021
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-05-25 21:25:48 +0530

    Update 25-05-2025.cpp

--- 2025 /05 - May/25-05-2025.cpp
+++ 2025 /05 - May/25-05-2025.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) {
+        // code here
+              int n = arr.size();
+        for(auto &it:arr)it *= it;
+        sort(arr.begin(), arr.end());
+        for(int i=n-1; i>1; i--){
+            int l=0, h = i-1;
+            if(i<n-1 && arr[i]==arr[i+1])continue;
+            while(l<h){
+                int sum = arr[l] + arr[h];
+                if(sum==arr[i])return true;
+                else if(sum<arr[i])l++;
+                else h--;
+            }
+        }
+        return false;
+    }
+};
