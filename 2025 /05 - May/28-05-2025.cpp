ba4918e229612d791181f9fb2069072dd2ca6782
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-05-28 21:41:54 +0530

    Update 28-05-2025.cpp

--- 2025 /05 - May/28-05-2025.cpp
+++ 2025 /05 - May/28-05-2025.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+  public:
+    bool ValidCorner(vector<vector<int> >& mat) {
+        // code here
+           int n = mat.size();
+        int m = mat[0].size();
+        int cnt=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                cnt=0;
+                for(int k = 0;k<m;k++){
+                    if((mat[i][k] & mat[j][k]) == 1){
+                        cnt++;
+                    }
+                    if(cnt>=2){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
