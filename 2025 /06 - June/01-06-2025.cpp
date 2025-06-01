da5fe0991a51f77961a24de8046a89713732d0cc
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-06-01 21:44:15 +0530

    Update 01-06-2025.cpp

--- 2025 /06 - June/01-06-2025.cpp
+++ 2025 /06 - June/01-06-2025.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
    int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int x) {
         int n = mat1.size();
-        int total = n * n;
+        int total = n * n; 
     
         int p1 = 0, p2 = total - 1; 
         int cnt = 0;
