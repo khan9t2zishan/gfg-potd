4675331e887fd87e922fe1091c52590518d6db66
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-06-03 21:28:57 +0530

    Update 03-06-2025.cpp

--- 2025 /06 - June/03-06-2025.cpp
+++ 2025 /06 - June/03-06-2025.cpp
@@ -1,7 +1,7 @@
 class Solution {
   public:
     int countSubstr(string& s, int k) {
-        // code here.
+        // code here. 
           int result{};
     for (size_t i{}, len(s.length()); i < len; i++) {
       int distinct{}, freq[26]{};
