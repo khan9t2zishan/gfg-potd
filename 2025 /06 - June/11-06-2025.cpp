06efa9910b770a97dd97376b3855080d930c4a6d
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-06-11 21:43:48 +0530

    Update 11-06-2025.cpp

--- 2025 /06 - June/11-06-2025.cpp
+++ 2025 /06 - June/11-06-2025.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int findLength(vector<int> &color, vector<int> &radius) {
         // code here 
-        vector<int> balls;
+        vector<int> balls; 
         int n = color.size(); 
         for(int i=0;i<n;i++){ 
             if(balls.empty()) balls.push_back(i);
