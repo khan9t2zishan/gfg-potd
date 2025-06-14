66ab301368a4a5a38e72c0c0607dfbe093c6bc23
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-06-14 21:41:31 +0530

    Update 14-06-2025.cpp

--- 2025 /06 - June/14-06-2025.cpp
+++ 2025 /06 - June/14-06-2025.cpp
@@ -1,1 +1,28 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left, *right;
 
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    bool solve(Node* l,Node* r){
+        if(l==NULL && r==NULL)return false;
+        if(l==NULL || r==NULL || l->data!=r->data)return true;
+        
+        
+        if(solve(l->left,r->right))return true;
+        if(solve(l->right,r->left))return true;
+        return false;
+    }
+    bool isSymmetric(Node* root) {
+        // Code here
+      return !solve(root->left,root->right);  
+    }
+};
