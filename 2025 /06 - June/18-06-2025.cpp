e9f35c881fc333ecd5e48233ba2a7adbc996bf46
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-06-18 22:21:28 +0530

    Update 18-06-2025.cpp

--- 2025 /06 - June/18-06-2025.cpp
+++ 2025 /06 - June/18-06-2025.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
      bool ispalindrome(string&s){   
         int i=0;   
-        int j=s.size()-1; 
+        int j=s.size()-1;  
         while(i<j){
             if(s[i]!=s[j]){
                 return false;
