e3ab140840268605a431acfc9e01ede709518379
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-06-24 23:02:11 +0530

    Update 24-06-2025.cpp

--- 2025 /06 - June/24-06-2025.cpp
+++ 2025 /06 - June/24-06-2025.cpp
@@ -3,7 +3,7 @@ class Solution {
     string maxSubseq(string& s, int k) {
         // code here
          stack<int>st;
-        int n=s.length();
+        int n=s.length(); 
         int cnt=0;   //To keep track on number of elements present in the stack at any point of time
         for(int i=0;i<n;i++){
             while(cnt>(i-k) && !st.empty() && s[st.top()]<s[i]){
