616dd7d85cd1393c05edb1b3314171ae2c31176f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-06-28 21:43:11 +0530

    Create 29-06-2025.cpp

---
+++ 2025 /06 - June/29-06-2025.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    vector<int> countLessEq(vector<int>& a, vector<int>& b) {
+        // code here
+         sort(b.begin(), b.end());
+        vector<int> ans;
+        for(auto &e : a){
+            ans.push_back(upper_bound(b.begin(), b.end(), e) - b.begin());
+        }
+        return ans;
+    }
+};
