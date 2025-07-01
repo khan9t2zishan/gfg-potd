6c717c343694e5a316c97ee4bab58bf6f90520b5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-01 23:15:26 +0530

    Update 01-07-2025.cpp

--- 2025 /07 - July/01-07-2025.cpp
+++ 2025 /07 - July/01-07-2025.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+  public:
+    int substrCount(string &s, int k) {
+        // code here
+        unordered_map<char,int>alp;
+        int n = s.size();
+        string str = "";
+        int ans = 0;
+        for(int i = 0 ; i<n;  i++){
+            str+=s[i];
+            alp[s[i]]++;
+            if(str.length()==k){
+                  if(alp.size()==k-1)ans++;
+                alp[str[0]]--;
+                if(alp[str[0]]==0)alp.erase(str[0]);
+                str.erase(0,1);
+            }
+            
+        }
+        return ans;
+    }
+};
