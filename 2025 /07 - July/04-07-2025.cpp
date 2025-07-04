77c4f24707fce7af19abefd929ab0d323c566ee4
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-04 22:10:41 +0530

    Update 04-07-2025.cpp

--- 2025 /07 - July/04-07-2025.cpp
+++ 2025 /07 - July/04-07-2025.cpp
@@ -1,1 +1,26 @@
+class Solution {
+  public:
+    int countAtMostK(vector<int> &arr, int k) {
+        // code here
+          unordered_map<int, int> mp;
+        int l = 0;
+        int n = arr.size();
+        int distinct_count = 0;
+        int not_distinct_subarr = 0;
 
+        for (int i = 0; i < n; i++) {
+            mp[arr[i]]++;
+            
+            if (mp[arr[i]] == 1)    distinct_count++;
+            
+            while (distinct_count > k) {
+                not_distinct_subarr += (n - i);
+                mp[arr[l]]--;
+                if (mp[arr[l]] == 0)    distinct_count--;
+                l++;
+            }
+        }
+
+        return ((n * (n + 1)) / 2) - not_distinct_subarr;
+    }
+};
