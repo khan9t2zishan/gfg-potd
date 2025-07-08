3fe42a27117af8f963854009e21586ce3f3d3c5a
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-08 22:40:36 +0530

    Update 08-07-2025.cpp

--- 2025 /07 - July/08-07-2025.cpp
+++ 2025 /07 - July/08-07-2025.cpp
@@ -1,1 +1,24 @@
+class Solution {
+  public:
+    vector<int> findGreater(vector<int>& arr) {
+        // code here
+              int n = arr.size();
+        map<int, int> freq;
+
+        for (auto it : arr) {
+            freq[it]++;
+        }
+        vector<int> res(n, -1);
+        stack<int> s;
+
+        for (int i = 0; i < n; i++) {
+            while (!s.empty() && freq[arr[i]] > freq[arr[s.top()]]) {
+                res[s.top()] = arr[i];
+                s.pop();
+            }
+            s.push(i);
+        }
+        return res;
+    }
+};
 
