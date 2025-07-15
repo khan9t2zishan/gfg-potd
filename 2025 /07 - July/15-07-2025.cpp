71243692f92059ac1e5017139c69a61f8a702a00
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-15 22:10:57 +0530

    Update 15-07-2025.cpp

--- 2025 /07 - July/15-07-2025.cpp
+++ 2025 /07 - July/15-07-2025.cpp
@@ -3,7 +3,7 @@ class Solution {
     bool divby13(string &s) {
         // code here
          int n = s.length(); 
-        if(n < 2) return false ;
+        if(n < 2) return false ; 
         int num = s[0]-'0'; 
         for (int i = 1 ; i < n ; i++){
             num *= 10; 
