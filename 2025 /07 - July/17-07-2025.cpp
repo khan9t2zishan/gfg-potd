c5008b84ab518023d41895cf3f0d06cf9ba48dfe
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-17 22:03:44 +0530

    Update 17-07-2025.cpp

--- 2025 /07 - July/17-07-2025.cpp
+++ 2025 /07 - July/17-07-2025.cpp
@@ -7,7 +7,7 @@ class Solution {
         int ans = 1e9;
     
         isPrime[0] = isPrime[1] = false;
-        for (int i = 2; i * i <= n; i++) {
+        for (int i = 2; i * i <= n; i++){
             if (isPrime[i]) {
                 for (int j = i * i; j <= n; j += i) 
                     isPrime[j] = false;
