96eb360935b0bd865682c4392a62d0d8935d9ae7
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-18 22:08:26 +0530

    Update 18-07-2025.cpp

--- 2025 /07 - July/18-07-2025.cpp
+++ 2025 /07 - July/18-07-2025.cpp
@@ -3,7 +3,7 @@ class Solution {
    int gcd(int a, int b){
         if(b == 0) return a;
         return gcd(b, a % b);
-    }
+    } 
     
     int lcm(int a, int b){
         return (a/gcd(a, b) * b);
