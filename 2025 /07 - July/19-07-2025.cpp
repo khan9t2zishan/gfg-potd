8ee9fccd890715bc40c741c102fa6ec4a9141643
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-19 21:34:47 +0530

    Update 19-07-2025.cpp

--- 2025 /07 - July/19-07-2025.cpp
+++ 2025 /07 - July/19-07-2025.cpp
@@ -1,1 +1,37 @@
-
+class Solution {
+  public:
+    bool isvowel(char ch){
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        return true;
+        return false;
+    }
+    
+    int combination(int st){
+        if(st==0)
+         return 0;
+         
+         int temp=1;
+         while(st!=0){
+         temp = temp * st;
+         st--;
+       }
+        return temp;
+    }
+    int vowelCount(string& s) {
+        // code here
+        unordered_map<char,int> mp;
+        for(char ch:s){
+            if(isvowel(ch))
+            mp[ch]+=1;
+        }
+        
+        int ans =0;
+        ans = combination(mp.size());
+        for(auto it:mp){
+            if(it.second>1)
+            ans = ans * it.second;
+        }
+        
+        return ans;
+    }
+};
