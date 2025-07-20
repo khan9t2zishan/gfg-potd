2d0cff4cb5578d0fa219cf39c21a9d8d127dc8b9
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-20 21:56:45 +0530

    Update 20-07-2025.cpp

--- 2025 /07 - July/20-07-2025.cpp
+++ 2025 /07 - July/20-07-2025.cpp
@@ -11,7 +11,7 @@ class Solution {
             }
         } 
         
-        int digits = arr.size();
+        int digits = arr.size(); 
         int not_containing = 1;
         for(int i = 1; i <= n; i++){
             if(i ==1){
