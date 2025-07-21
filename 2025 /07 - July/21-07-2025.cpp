20d58155cbdeface36c836e1736e2b2be99f166b
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-21 22:30:46 +0530

    Update 21-07-2025.cpp

--- 2025 /07 - July/21-07-2025.cpp
+++ 2025 /07 - July/21-07-2025.cpp
@@ -5,7 +5,7 @@ class Solution {
         int n = arr.size(), m = 0; 
         for(int i = 0; i < n; i++) m = max(m, arr[i]);
         vector<int> freq(m + 1, 0), dp(m + 1, 0);
-        for(int i = 0; i < n; i++) freq[arr[i]]++;
+        for(int i = 0; i < n; i++) freq[arr[i]]++; 
         for(int i = m; i >= 1; i--) {
             int c = 0;
             for(int j = i; j <= m; j += i) {
