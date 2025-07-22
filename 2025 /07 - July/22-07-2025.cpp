fc94d4df8aa951679e1347f8c9da151202003fad
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-22 21:50:46 +0530

    Update 22-07-2025.cpp

--- 2025 /07 - July/22-07-2025.cpp
+++ 2025 /07 - July/22-07-2025.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+  public:
+    int missingNumber(vector<int> &arr) {
+        // code here
+        unordered_set<int>myset;
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]>0)
+            myset.insert(arr[i]);
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(myset.find(i)==myset.end()){
+                return i;
+            }
+        }
+        return n+1;
+    }
+};
