76d6a0788c9e0b2423aa516bdd4a15a6c807008c
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-23 22:59:49 +0530

    Update 23-07-2025.cpp

--- 2025 /07 - July/23-07-2025.cpp
+++ 2025 /07 - July/23-07-2025.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int subarraySum(vector<int>& arr) {
         // code here
-        int ans = 0 ; 
+        int ans = 0 ;  
         int n = arr.size();
          
         for(int i=0;i<n;i++){
