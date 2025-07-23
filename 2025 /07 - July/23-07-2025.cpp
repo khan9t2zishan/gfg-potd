fc62fbf71855651cbdd1095065fd21d811b9d45f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-23 23:00:05 +0530

    Update 23-07-2025.cpp

--- 2025 /07 - July/23-07-2025.cpp
+++ 2025 /07 - July/23-07-2025.cpp
@@ -3,7 +3,7 @@ class Solution {
     int subarraySum(vector<int>& arr) {
         // code here
         int ans = 0 ;  
-        int n = arr.size();
+        int n = arr.size(); 
          
         for(int i=0;i<n;i++){ 
             int left = i+1;
