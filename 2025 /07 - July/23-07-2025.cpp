097524a0becabfdc60dc1dd356690228916aa8fe
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-23 22:57:40 +0530

    Update 23-07-2025.cpp

--- 2025 /07 - July/23-07-2025.cpp
+++ 2025 /07 - July/23-07-2025.cpp
@@ -4,7 +4,7 @@ class Solution {
         // code here
         int ans = 0 ;
         int n = arr.size();
-        
+         
         for(int i=0;i<n;i++){
             int left = i+1;
             int right = n-i;
