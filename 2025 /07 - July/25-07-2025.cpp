6885c6c06caf6ce593c0c40761182032109adad5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-07-25 23:26:16 +0530

    Update 25-07-2025.cpp

--- 2025 /07 - July/25-07-2025.cpp
+++ 2025 /07 - July/25-07-2025.cpp
@@ -4,7 +4,7 @@ class Solution {
         // code here
            int curr_max=arr[0], max_sum=arr[0];
         int curr_min=arr[0], min_sum=arr[0];
-        int total= arr[0];
+        int total= arr[0]; 
         for(int i=1;i<arr.size();i++){
             curr_max=max(arr[i],curr_max+arr[i]);
             max_sum=max(max_sum,curr_max);
