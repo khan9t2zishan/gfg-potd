c48b0e5fa58ac85b1289d72839627195b880b924
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-08-09 20:48:44 +0530

    Update 09-08-2025.cpp

--- 2025 /08 - August/09-08-2025.cpp
+++ 2025 /08 - August/09-08-2025.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int getLongestPrefix(string &s) {
         // code here
-        int n = s.length(), ans = -1;  
+        int n = s.length(), ans = -1;   
         vector<int> z(n, 0);
         for(int i = 1, l = -1; i < n; i++) {
             if(l != -1 && i < l + z[l]) z[i] = min(z[i - l], l + z[l] - i);
