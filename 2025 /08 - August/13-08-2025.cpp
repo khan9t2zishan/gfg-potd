a3e682cc627f0b69c5f921d7c501f65d0dda8729
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-08-13 21:09:54 +0530

    Update 13-08-2025.cpp

--- 2025 /08 - August/13-08-2025.cpp
+++ 2025 /08 - August/13-08-2025.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-    int minSoldiers(vector<int>& arr, int k) {
+    int minSoldiers(vector<int>& arr, int k) { 
         // code here
        for(int i = 0;i<arr.size();i++){
             arr[i] = arr[i] + k;
