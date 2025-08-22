cb7fddac26bb42199b41babf7d4104b14f4cd5da
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-08-22 22:31:49 +0530

    Update 22-08-2025.cpp

--- 2025 /08 - August/22-08-2025.cpp
+++ 2025 /08 - August/22-08-2025.cpp
@@ -5,7 +5,7 @@ class Solution {
         vector<int>val;
         for(int i=0;i<mat.size();i++)
             for(int j=0;j<mat[0].size();j++) val.push_back(mat[i][j]);
-        sort(val.begin(),val.end());
+        sort(val.begin(),val.end()); 
         return val[(mat.size()*mat[0].size())/2];
     }
 };
