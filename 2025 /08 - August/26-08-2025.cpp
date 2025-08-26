e0b9848397c109f19c028bacf3d2ca90db73f228
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-08-26 22:44:27 +0530

    Update 26-08-2025.cpp

--- 2025 /08 - August/26-08-2025.cpp
+++ 2025 /08 - August/26-08-2025.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+  public:
+    bool isSubSeq(string& s1, string& s2) {
+        // code here
+       int n = s2.size();
+       int i=0,j=0;
+       while(j<n){
+           if(s1[i] == s2[j])
+               i++;
+            j++;
+       }
+       return i == s1.size();
+    }
+};
