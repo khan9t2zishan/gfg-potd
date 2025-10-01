4f8d5e959165ebeb4d1a183aa206443222140274
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-10-01 13:48:48 +0530

    Update 01-10-2025.cpp

--- 2025 /10 - October/01-10-2025.cpp
+++ 2025 /10 - October/01-10-2025.cpp
@@ -4,7 +4,7 @@ class Solution {
         // code here
         vector<vector<int>>ans;
         sort(arr.begin(), arr.end()); 
-        ans.push_back(arr);
+        ans.push_back(arr); 
         while(next_permutation(arr.begin() , arr.end())) ans.push_back(arr); //next_permutaion is an funtion that creates the permutation automatically in the greater lexicographic way.
         return ans;
     }
