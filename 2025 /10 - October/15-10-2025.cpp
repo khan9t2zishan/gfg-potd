de81396568e38f871da1a8dcbab154a31d7f740e
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-10-15 21:29:05 +0530

    Update 15-10-2025.cpp

--- 2025 /10 - October/15-10-2025.cpp
+++ 2025 /10 - October/15-10-2025.cpp
@@ -1,1 +1,26 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
 
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int kthSmallest(Node *root, int &k) {
+        // code here
+        if(root == NULL) return -1;
+        int tmp = kthSmallest(root -> left, k);
+        if(tmp != -1) return tmp;
+        k--;
+        if(k == 0) return root -> data;
+        return kthSmallest(root -> right, k);
+    }
+};
