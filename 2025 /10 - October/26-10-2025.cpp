53ea85b4f20aa0d7e22a350be4b111ce44a5c0d2
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-10-26 23:23:39 +0530

    Update 26-10-2025.cpp

--- 2025 /10 - October/26-10-2025.cpp
+++ 2025 /10 - October/26-10-2025.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        // code here
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto it:arr){
+            pq.push(it);
+        }
+        int sum=0;
+        while(pq.size()>1){
+            int top1=pq.top(); pq.pop();
+            int top2=pq.top(); pq.pop();
+            sum+=top1+top2;
+            pq.push(top1+top2);
+        }
+        return sum;
+    }
+};
