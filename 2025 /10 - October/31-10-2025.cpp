4b262dc16883ebddcf5b656093ec1785c8c7dcaf
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-10-31 22:46:54 +0530

    Update 31-10-2025.cpp

--- 2025 /10 - October/31-10-2025.cpp
+++ 2025 /10 - October/31-10-2025.cpp
@@ -6,7 +6,7 @@ class Solution {
         int m = edges.size(), ans = V + 1;
          
         vector<vector<int>> adj(V);
-        
+          
         for(int i = 0; i < m; i++) {
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
