054d2429ec6dd379fe81914c715ad939dc15cf6f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-11-01 22:12:37 +0530

    Update 01-11-2025.cpp

--- 2025 /11 - November/01-11-2025.cpp
+++ 2025 /11 - November/01-11-2025.cpp
@@ -7,7 +7,7 @@ class Solution {
             adj[p[1]].push_back(p[0]);
             indegree[p[0]]++;
         }
-        queue<int> q;
+        queue<int> q; 
         for (int i = 0; i < n; i++)
             if (indegree[i] == 0){
                 q.push(i);
