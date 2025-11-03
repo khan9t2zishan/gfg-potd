4e7cc0cd7b9c5f616a718803f6b894cc6f6e5892
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-11-03 21:26:45 +0530

    Update 03-11-2025.cpp

--- 2025 /11 - November/03-11-2025.cpp
+++ 2025 /11 - November/03-11-2025.cpp
@@ -5,7 +5,7 @@ class Solution {
         for(int i = 0;i<edges.size();i++){
             int u = edges[i][0];
             int v = edges[i][1];
-            
+             
             adj[u].push_back(v);
         }
         return adj;
