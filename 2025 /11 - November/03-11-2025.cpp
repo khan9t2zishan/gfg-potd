0a30227d92f6cf626baef195301896a506f900a9
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-11-03 21:27:34 +0530

    Safe States

--- 2025 /11 - November/03-11-2025.cpp
+++ 2025 /11 - November/03-11-2025.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-    vector<vector<int>> GetAdj(int V, vector<vector<int>>& edges){
+    vector<vector<int>> GetAdj(int V, vector<vector<int>>& edges){ 
         vector<vector<int>> adj(V);
         for(int i = 0;i<edges.size();i++){
             int u = edges[i][0]; 
