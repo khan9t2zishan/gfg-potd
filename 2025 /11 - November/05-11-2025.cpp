cb62c3453209e2cfa9b3dac81ec9608974c5f296
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-11-06 11:06:19 +0530

    Update 05-11-2025.cpp

--- 2025 /11 - November/05-11-2025.cpp
+++ 2025 /11 - November/05-11-2025.cpp
@@ -1,17 +1,14 @@
-class Solution {
-  public:
-        int minSquares(int n) {
-        // Code here
-        int[] dp = new int[n + 1];
-        dp[0] = 0;  
-  
-        for (int i = 1; i <= n; i++) {
-            dp[i] = i; 
-            for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
-            }
+class Solution{
+public:
+int minSquares(int n){
+    int dp[n+1];
+    dp[0] = 0; dp[1] = 1;
+    for(int i = 2 ; i<=n ; i++){
+        dp[i] = INT_MAX;
+        for(int j=1 ; j<=floor(sqrt(i)) ; j++){
+            dp[i] = min(dp[i] , dp[i - (j*j)]+1);
         }
-
-        return dp[n];
     }
+    return dp[n];
+}
 };
