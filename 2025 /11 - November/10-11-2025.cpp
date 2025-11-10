244021a86da6eaf98e73f2b6fefc09e6f59e2548
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-11-10 20:04:52 +0530

    Stock Buy and Sell with Cooldown

--- 2025 /11 - November/10-11-2025.cpp
+++ 2025 /11 - November/10-11-2025.cpp
@@ -4,7 +4,7 @@ class Solution {
         if(i>=n) return 0;
         if(dp[i][s]!=-1) return dp[i][s];
         
-        int buy=0, sell=0;
+        int buy=0, sell=0; 
         
         if(s==0){ 
             buy=max(solve(i+1, n, s, arr, dp), (-1)*arr[i]+solve(i+1, n, 1-s, arr, dp));
