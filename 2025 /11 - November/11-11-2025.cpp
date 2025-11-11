e86c756c9e0eb239fbc1d993e4939d8940c1850a
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-11-11 11:18:41 +0530

    Shortest Common Supersequence

--- 2025 /11 - November/11-11-2025.cpp
+++ 2025 /11 - November/11-11-2025.cpp
@@ -5,7 +5,7 @@ class Solution {
         vector<vector<int>> dp(s1.size()+1,vector<int> (s2.size()+1,0));
         for(int i=1;i<=s1.size();i++){ 
             for(int j=1;j<=s2.size();j++){
-                if(s1[i-1]==s2[j-1]){
+                if(s1[i-1]==s2[j-1]){ 
                     dp[i][j]=dp[i-1][j-1]+1;
                 }
                 else{
