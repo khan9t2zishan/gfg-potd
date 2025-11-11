a97235b95dce2e9d0f3702b31971450124e198b3
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-11-11 11:18:26 +0530

    Update 11-11-2025.cpp

--- 2025 /11 - November/11-11-2025.cpp
+++ 2025 /11 - November/11-11-2025.cpp
@@ -3,7 +3,7 @@ class Solution {
     int minSuperSeq(string &s1, string &s2) {
         // code here
         vector<vector<int>> dp(s1.size()+1,vector<int> (s2.size()+1,0));
-        for(int i=1;i<=s1.size();i++){
+        for(int i=1;i<=s1.size();i++){ 
             for(int j=1;j<=s2.size();j++){
                 if(s1[i-1]==s2[j-1]){
                     dp[i][j]=dp[i-1][j-1]+1;
