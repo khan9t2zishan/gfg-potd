ba6335b66ee540426f5a76543222d1f766edc9b7
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-11-27 22:31:28 +0530

    All Subsets Xor Sum

--- 2025 /11 - November/27-11-2025.cpp
+++ 2025 /11 - November/27-11-2025.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int subsetXORSum(vector<int>& arr) {
         // code here
-        int OR =0 , n = arr.size();
+         int OR =0 , n = arr.size();
          for(int x : arr) OR |= x;
          return OR * (pow(2,n-1)); 
     }
