65fc441d6f859b92a26bc977408ce8b9ee99fc70
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-02 21:42:49 +0530

    Maximise String Score

--- 2025 /12 - December/02-12-2025.cpp
+++ 2025 /12 - December/02-12-2025.cpp
@@ -5,7 +5,7 @@ public:
 
         // Build adjacency: can[from] contains target characters allowed from 'from'
         vector<vector<int>> can(26);
-        for (auto &pr : jumpsPairs) {
+        for (auto &pr : jumpsPairs) { 
             int a = pr[0] - 'a'; 
             int b = pr[1] - 'a';
             can[a].push_back(b);
