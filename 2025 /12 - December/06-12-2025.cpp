4d4df5331e37e794ceeabcbad28df066f7b8d424
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-06 22:17:50 +0530

    Optimal Strategy For A Game

--- 2025 /12 - December/06-12-2025.cpp
+++ 2025 /12 - December/06-12-2025.cpp
@@ -4,7 +4,7 @@ public:
         int n = arr.size();
         vector<vector<long long>> dp(n, vector<long long>(n));
         for(int i=0;i<n;i++) dp[i][i]=arr[i];
-        for(int len=2;len<=n;len++){
+        for(int len=2;len<=n;len++){ 
             for(int i=0;i+len-1<n;i++){
                 int j=i+len-1; 
                 long long left = arr[i] + min((i+2<=j?dp[i+2][j]:0),(i+1<=j-1?dp[i+1][j-1]:0));
