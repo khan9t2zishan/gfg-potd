a20f522967ba3cb93f3afed5196265166b006b60
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-10 22:01:14 +0530

    Missing And Repeating

--- 2025 /12 - December/10-12-2025.cpp
+++ 2025 /12 - December/10-12-2025.cpp
@@ -5,7 +5,7 @@ class Solution {
         int n = arr.size();
         int f = 0 , s = n; 
         vector<int> temp (n+1 , 0);
-        for(int it : arr) temp[it]++;  
+        for(int it : arr) temp[it]++;   
         for(int i = 1; i <= n ; i++){
             if(temp[i] == 2) f = i;
             if(temp[i] == 0) s = i;
