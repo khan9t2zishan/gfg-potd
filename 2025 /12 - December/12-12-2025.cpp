d8fc5460bd36db8514835e8188f49c2dcb546263
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-12 22:16:45 +0530

    Transpose of Matrix

--- 2025 /12 - December/12-12-2025.cpp
+++ 2025 /12 - December/12-12-2025.cpp
@@ -9,7 +9,7 @@ class Solution {
             for (int j = 0; j < i; j++)
             {
                 swap(mat[i][j], mat[j][i]);
-            }
+            } 
         }
         
         return mat;
