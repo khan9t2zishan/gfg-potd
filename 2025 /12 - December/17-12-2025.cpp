28fb87c10805f6b18816839ebb205e9ebded2306
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-17 22:16:23 +0530

    Update 17-12-2025.cpp

--- 2025 /12 - December/17-12-2025.cpp
+++ 2025 /12 - December/17-12-2025.cpp
@@ -7,7 +7,7 @@ class Solution {
         int start = arr[0][0],end = arr[0][1];
         for(int i=1;i<arr.size();i++){
             if(end>=arr[i][0]){
-                end = max(end,arr[i][1]);
+                end = max(end,arr[i][1]); 
                 continue;
             } 
             res.push_back({start,end});
