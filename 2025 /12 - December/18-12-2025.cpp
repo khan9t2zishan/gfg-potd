c4df239a584343d41968f19013526a7df6019697
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-18 21:35:10 +0530

    Update 18-12-2025.cpp

--- 2025 /12 - December/18-12-2025.cpp
+++ 2025 /12 - December/18-12-2025.cpp
@@ -8,7 +8,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (arr[i] % 2 != 0) {
                 swap(arr[i], arr[idx]); 
-                idx++;
+                idx++; 
             }
         }
         
