d1271442510e1b1ba383ebdaf8ba6782f416224d
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-18 21:35:19 +0530

    Update 18-12-2025.cpp

--- 2025 /12 - December/18-12-2025.cpp
+++ 2025 /12 - December/18-12-2025.cpp
@@ -4,7 +4,7 @@ class Solution {
         // code here
         int n = arr.size();
         
-        int idx = 0;
+        int idx = 0; 
         for (int i = 0; i < n; i++) {
             if (arr[i] % 2 != 0) {
                 swap(arr[i], arr[idx]); 
