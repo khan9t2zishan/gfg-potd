9a39b7deb077ad855d9c8548f8f31df43e021320
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-22 22:55:33 +0530

    Row with max 1s

--- 2025 /12 - December/22-12-2025.cpp
+++ 2025 /12 - December/22-12-2025.cpp
@@ -6,7 +6,7 @@ class Solution {
         int n = arr.size();
         int m = arr[0].size(); 
         
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++){ 
             for(int j=0;j<n;j++){
                 if(arr[j][i] == 1)
                 return j;
