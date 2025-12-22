3b2574a420e40b032b0fc81e14668051284887d7
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-22 22:55:23 +0530

    Update 22-12-2025.cpp

--- 2025 /12 - December/22-12-2025.cpp
+++ 2025 /12 - December/22-12-2025.cpp
@@ -4,7 +4,7 @@ class Solution {
     int rowWithMax1s(vector<vector<int>> &arr) {
         // code here 
         int n = arr.size();
-        int m = arr[0].size();
+        int m = arr[0].size(); 
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
