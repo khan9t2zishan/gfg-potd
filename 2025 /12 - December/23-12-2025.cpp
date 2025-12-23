f23b3dae98e151bfbf7b45f2de5e6fcbe0c9cc00
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-23 18:53:51 +0530

    Update 23-12-2025.cpp

--- 2025 /12 - December/23-12-2025.cpp
+++ 2025 /12 - December/23-12-2025.cpp
@@ -1,1 +1,33 @@
+class Solution {
+  public:
+    vector<int> cntInRange(vector<int> &arr, vector<vector<int>> &queries) {
+        // code here
+        int a,b;
+        vector<int> v;
+        sort(arr.begin(),arr.end());
 
+        for(int i=0;i<queries.size();i++)
+        {
+            a = queries[i][0];
+
+            b = queries[i][1];
+
+            int count = 0;
+
+            
+            int left = lower_bound(arr.begin(),arr.end(),a) - arr.begin();
+
+            int right = upper_bound(arr.begin(),arr.end(),b) - arr.begin();
+
+            
+
+            count = right-left;
+
+            v.push_back(count);
+
+        }
+
+        return v;
+
+    }
+};
