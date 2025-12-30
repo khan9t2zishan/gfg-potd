b8dab0554a7105eb3d8f4f9aeba5e91ead9e6fd9
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2025-12-30 21:45:07 +0530

    Minimum time to fulfil all orders

--- 2025 /12 - December/28-12-2025.cpp
+++ 2025 /12 - December/28-12-2025.cpp
@@ -1,1 +1,32 @@
-    
+class Solution {
+  public:
+    int valid(vector<int>& ranks, int n,int time){
+        int count = 0;
+        for(auto it:ranks){
+            int totalTime = it;
+            int currTime = it;
+            while(totalTime<=time){
+                count++;
+                currTime += it; 
+                totalTime += currTime;
+            }
+            if(count >= n) return true;
+        }
+        return count>=n;
+    }
+    int minTime(vector<int>& ranks, int n) {
+        // code here
+        int start = 0;
+        int end = INT_MAX;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(valid(ranks,n,mid)){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return end;
+    }
+};    
