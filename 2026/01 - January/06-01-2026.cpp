c5ac016fd51f0508628db1d6caea28707b1216e5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-01-06 22:22:15 +0530

    Update 06-01-2026.cpp

--- 2026/01 - January/06-01-2026.cpp
+++ 2026/01 - January/06-01-2026.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+  public:
+    int maxSubarrayXOR(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size(), ans = 0;
+        for(int i = 0, x = 0; i < n; i++) {
+            x ^= arr[i];
+            if(i - k >= 0) x ^= arr[i - k];
+            if(i >= k - 1) ans = max(ans, x);
+        }
+        return ans;
+    }
+};
