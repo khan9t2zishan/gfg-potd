b0a2d7bf7a12f05b0b501ce90a4928700bfe3479
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-01-11 22:37:52 +0530

    Update 11-01-2026.cpp

--- 2026/01 - January/11-01-2026.cpp
+++ 2026/01 - January/11-01-2026.cpp
@@ -1,1 +1,46 @@
+class Solution {
+  public:
+    string minWindow(string& s1, string& s2) {
+        // Code here
+        int i = 0, j = 0; 
+        int minlen = INT_MAX ; 
+        int st = -1 ;
+        for(; i < s1.length(); i++)
+        {
+            if(s1[i] == s2[j]) {
+                j++; 
+            }
+            
+            if(j == s2.length()) 
+            {
+                
+                int ed = i; 
+                j--; 
+                
+                while(j >= 0)
+                {
+                    if(s1[i] == s2[j])
+                        j--;
+                    i--;
+                }
+                
+                i++; 
+                j++; 
+                
+                int currlen = ed - i + 1;  
+                
+                if(currlen < minlen) 
+                {
+                    minlen = currlen ;
+                    st = i ;
+                }
+                
+            }
+        }
+        
+        if(minlen == INT_MAX) return "" ;
+        
+        return s1.substr(st, minlen) ;
+    }
+};
 
