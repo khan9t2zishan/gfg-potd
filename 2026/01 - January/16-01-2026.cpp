704f65316b9208d38b9897ab877e8199b86e5d86
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-01-16 23:13:12 +0530

    Update 16-01-2026.cpp

--- 2026/01 - January/16-01-2026.cpp
+++ 2026/01 - January/16-01-2026.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int minMen(vector<int>& arr) {
         // code here
-        int n = arr.size();
+        int n = arr.size(); 
         vector<int> dp(n, -1);
         for(int i = 0; i < n; i++) {
             if(arr[i] != -1) {
