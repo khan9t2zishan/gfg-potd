3e1b85dd9384dabf0769c7695305eb27ca7c6dd7
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-01-26 22:51:10 +0530

    Update 26-01-2026.cpp

--- 2026/01 - January/26-01-2026.cpp
+++ 2026/01 - January/26-01-2026.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+  public:
+    vector<vector<int>> permuteDist(vector<int>& arr) {
+        // code here
+        vector<vector<int>> ans; 
+        backtrack(arr, 0, ans); 
+        return ans;
+    }
+    
+    void backtrack(vector<int>& arr, int idx, vector<vector<int>>& ans) { 
+        if (idx == arr.size()) { 
+            ans.push_back(arr); 
+            return; 
+        } 
+        for (int i = idx; i < arr.size(); i++) { 
+            swap(arr[idx], arr[i]); 
+            backtrack(arr, idx + 1, ans); 
+            swap(arr[idx], arr[i]); 
+        } 
+    }
+};
