9743dc789e1625c0abd8a02125be8c9e3d23c73d
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-03 22:42:01 +0530

    Stock Buy and Sell – Max one Transaction Allowed

--- 2026/02 - February/03-02-2026.cpp
+++ 2026/02 - February/03-02-2026.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int maxProfit(vector<int> &prices) {
         // code here
-        int minPrice = prices[0];
+        int minPrice = prices[0]; 
         int maxProfit = 0;
 
         for (int i = 1; i < prices.size(); i++) {
