bccfe4f3585efb969e374fd7010427f567100a3e
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-13 21:40:17 +0530

    Update 13-02-2026.cpp

--- 2026/02 - February/13-02-2026.cpp
+++ 2026/02 - February/13-02-2026.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+    int check(int& d,int num){
+        int sum = 0;
+        int temp = num;
+        while(temp>0){
+            int d = temp%10;
+            sum += d;
+            temp /= 10;
+        }
+        return d <= (num-sum);
+    }
+  public:
+    int getCount(int n, int d) {
+        // code here
+        if(n < 10) return 0;
+        int low = 10;
+        int high = n;
+        int possible = 0;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(check(d,mid)){
+                possible = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        if(!possible) return 0;
+        return n-possible+1;
+    }
+};
