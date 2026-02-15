ef32b55849d574cd8006706447cbe2818967d795
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-15 21:23:42 +0530

    Update 15-02-2026.cpp

--- 2026/02 - February/15-02-2026.cpp
+++ 2026/02 - February/15-02-2026.cpp
@@ -3,7 +3,7 @@ class Solution {
     int findMinDiff(vector<int>& a, int m) {
         // code here
         int n=a.size(); 
-        sort(a.begin(),a.end());
+        sort(a.begin(),a.end()); 
         int mini=INT_MAX;
         for(int i=0;i<n;i++){
             int j=i+m-1;
