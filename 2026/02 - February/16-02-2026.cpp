24cf5581c211eb253a1102781e6f98238e5cb6f3
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-16 21:20:13 +0530

    Update 16-02-2026.cpp

--- 2026/02 - February/16-02-2026.cpp
+++ 2026/02 - February/16-02-2026.cpp
@@ -1,7 +1,7 @@
 class Solution {
   public:
     bool canAttend(vector<vector<int>> &arr) {
-        // Code Here
+        // Code Here 
         sort(arr.begin(), arr.end());
         for(int i = 1;i<arr.size();i++){ 
             if(arr[i-1][1]>arr[i][0])return false;
