0b131269c65c8dc195ed10950d54658939ba1934
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-21 12:13:19 +0530

    Update 21-02-2026.cpp

--- 2026/02 - February/21-02-2026.cpp
+++ 2026/02 - February/21-02-2026.cpp
@@ -4,7 +4,7 @@ class Solution {
         // code here
         int n=citations.size();
         sort(citations.begin(),citations.end());
-        int h=0;
+        int h=0; 
         for(int i=citations.size()-1;i>=0;i--){
             int ch=min(n-i,citations[i]);
             h=max(h,ch);
