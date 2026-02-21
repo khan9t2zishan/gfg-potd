34529e81530eea413b994626fa386dbeb0d130ca
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-21 12:13:10 +0530

    Find H-Index

--- 2026/02 - February/21-02-2026.cpp
+++ 2026/02 - February/21-02-2026.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+  public:
+    int hIndex(vector<int>& citations) {
+        // code here
+        int n=citations.size();
+        sort(citations.begin(),citations.end());
+        int h=0;
+        for(int i=citations.size()-1;i>=0;i--){
+            int ch=min(n-i,citations[i]);
+            h=max(h,ch);
+        }
+        return h;
+    }
+};
