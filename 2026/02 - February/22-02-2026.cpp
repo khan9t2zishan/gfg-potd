1f2c97a282e7a3ed498a0a512b99416f22213ae4
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-22 22:38:37 +0530

    Update 22-02-2026.cpp

--- 2026/02 - February/22-02-2026.cpp
+++ 2026/02 - February/22-02-2026.cpp
@@ -1,1 +1,19 @@
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        // code here
+        long ct = 0;
+        int c = 0;
+        unordered_map<int, int> mp;
+        mp.reserve(arr.size()); 
+        mp[0] = 1;              
 
+        for (const int& it : arr) {
+            c ^= it;
+            ct += mp[c ^ k];    
+            mp[c]++;
+        }
+
+        return ct;
+    }
+};
