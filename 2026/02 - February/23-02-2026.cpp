b66c3bf93ef0b128aadf3d3e941c0c6fa0922174
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-23 22:20:55 +0530

    Update 23-02-2026.cpp

--- 2026/02 - February/23-02-2026.cpp
+++ 2026/02 - February/23-02-2026.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        // code here
+        vector<int>ans;
+        set<int>s;
+        for(int i=0;i<a.size();i++){
+            s.insert(a[i]);
+        }
+        for(int i=0;i<b.size();i++){
+            s.insert(b[i]);
+        }
+        for(int x:s){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
