d64b6f15ca0d561569f9fa660d6dc729fdef708a
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-02-25 20:00:23 +0530

    Update 25-02-2026.cpp

--- 2026/02 - February/25-02-2026.cpp
+++ 2026/02 - February/25-02-2026.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+  public:
+    int longestSubarray(vector<int> &arr, int k) {
+        // Code here
+        int n = arr.size(), ans = 0, ct = 0;
+        unordered_map<int, int> mp;
+        
+        for(int i=0; i<n; i++) {
+            if(arr[i]>k) ct++;
+            else ct--;
+            
+            if(ct > 0) ans = max(ans, i+1);
+            else if(mp.find(ct-1)!=mp.end()) ans = max(ans, i-mp[ct-1]);
+            
+            if(mp.find(ct)==mp.end()) mp[ct]=i;
+        }
+        return ans;
+    }
+};
