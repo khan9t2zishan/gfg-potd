754252752da861d8cc2f9c7de050a83f20105bee
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-01 20:35:28 +0530

    Update 01-03-2026.cpp

--- 2026/03 - March/01-03-2026.cpp
+++ 2026/03 - March/01-03-2026.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int pos=0;
+        int n=arr.size();
+        //shifting of non zero elements 
+        for(int i =0;i<n;i++){
+           if(arr[i]!=0){
+               arr[pos]=arr[i];
+               pos++;
+           } 
+        }
+        //filling the remaining position with zeroes
+        while(pos<n){
+            arr[pos]=0;
+            pos++;
+        }
+    }
+};
