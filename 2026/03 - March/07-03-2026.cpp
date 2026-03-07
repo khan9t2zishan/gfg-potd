44ab1319ef653003972b9c88cc0f85b16cafc843
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-07 22:21:53 +0530

    Dice throw

--- 2026/03 - March/07-03-2026.cpp
+++ 2026/03 - March/07-03-2026.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int noOfWays(int m, int n, int x) {
         // code here
-        vector<vector<int>>dp(n+ 1,vector<int>(x+1, 0));  
+        vector<vector<int>>dp(n+ 1,vector<int>(x+1, 0));   
         dp[0][0]=1; 
         for(int i=1;i<=n;++i){
             for(int j=1;j<=x;++j){
