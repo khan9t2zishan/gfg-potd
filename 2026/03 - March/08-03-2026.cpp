cb0ea2c71f4555adee3c9de545d940223b6af3c3
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-08 22:39:48 +0530

    Update 08-03-2026.cpp

--- 2026/03 - March/08-03-2026.cpp
+++ 2026/03 - March/08-03-2026.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) {
+        // code here
+        unordered_set<int> st;
+        for(auto &e : arr){
+            st.insert(e*e);
+        }
+        for(auto &a : st){
+            for(auto &b : st){
+                if(a != b){
+                    if(st.count(a + b)) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
