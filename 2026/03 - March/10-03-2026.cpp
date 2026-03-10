7537de5a94648b461343029da1434ee38fc9e876
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-10 21:53:13 +0530

    Update 10-03-2026.cpp

--- 2026/03 - March/10-03-2026.cpp
+++ 2026/03 - March/10-03-2026.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        stack<int> st;
+        int res=0;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+            int next=st.empty()?n:st.top();
+            res+=(next-i);
+            st.push(i);
+        }
+        return res;
+    }
+};
