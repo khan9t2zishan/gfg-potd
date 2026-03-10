88a6630e2eda109f27180540c13c70407b5d63b2
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-10 21:54:08 +0530

    Subarrays with First Element Minimum

--- 2026/03 - March/10-03-2026.cpp
+++ 2026/03 - March/10-03-2026.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-    int countSubarrays(vector<int> &arr) {   
+    int countSubarrays(vector<int> &arr) {    
         // code here
         int n=arr.size();
         stack<int> st;
