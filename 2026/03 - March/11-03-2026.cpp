be35f2fa935c8e1eb2ba81cb4dbb15189704386c
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-11 22:33:14 +0530

    Update 11-03-2026.cpp

--- 2026/03 - March/11-03-2026.cpp
+++ 2026/03 - March/11-03-2026.cpp
@@ -1,1 +1,37 @@
+class Solution {
+  public:
+    int sumSubMins(vector<int> &arr) {
+        // code here
+        const int MOD = 1e9 + 7;
+        long long ans = 0;
+        int n = arr.size();
+        
+        stack<int> st;
+        st.push(-1); // sentinel
 
+        for (int i = 0; i < n; i++) {
+            while (st.top() != -1 && arr[i] < arr[st.top()]) {
+                int mid = st.top();
+                st.pop();
+
+                int left = mid - st.top();
+                int right = i - mid;
+
+                ans = (ans + 1LL * arr[mid] * left * right) % MOD;
+            }
+            st.push(i);
+        }
+
+        while (st.top() != -1) {
+            int mid = st.top();
+            st.pop();
+
+            int left = mid - st.top();
+            int right = n - mid;
+
+            ans = (ans + 1LL * arr[mid] * left * right) % MOD;
+        }
+
+        return ans;
+    }
+};
