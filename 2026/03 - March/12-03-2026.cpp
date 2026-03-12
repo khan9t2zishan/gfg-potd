e7c843a37f767a008e6ced61b6e3fef5d7c3b8e0
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-12 22:07:04 +0530

    Update 12-03-2026.cpp

--- 2026/03 - March/12-03-2026.cpp
+++ 2026/03 - March/12-03-2026.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int kBitFlips(vector<int>& arr, int k) {
+        int n = arr.size();
+        int flip = 0, ans = 0;
 
+        for(int i = 0; i < n; i++) {
+            
+            if(i >= k && arr[i-k] == 2)
+                flip ^= 1;
+
+            if((arr[i] ^ flip) == 0) {
+
+                if(i + k > n)
+                    return -1;
+
+                ans++;
+                flip ^= 1;
+                arr[i] = 2;
+            }
+        }
+
+        return ans;
+    }
+};
