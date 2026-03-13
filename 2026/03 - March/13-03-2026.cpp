3c120eeba7d1aad42072e15ae9fbc789cca07ecd
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-13 21:58:29 +0530

    Update 13-03-2026.cpp

--- 2026/03 - March/13-03-2026.cpp
+++ 2026/03 - March/13-03-2026.cpp
@@ -1,1 +1,43 @@
+class Solution{
+  public:
+    void solve(string s, int i, vector<string> &ans, string temp, int count) {
+    if (count > 4) return;  // If more than 4 segments, it's invalid
+    
+    if (i == s.size()) {
+        if (count == 4) {
+            ans.push_back(temp);
+        }
+        return;
+    }
+    
+    string t = "";
+    int n = s.size();
+    for (int ind = i; ind < min(n, i + 3); ind++) {
+        t += s[ind];
+        
+        // If the segment is valid (no leading zeros unless it's "0" and <= 255)
+        if ((t.size() > 1 && t[0] == '0') || stoi(t) > 255) break;
+        
+        if (!temp.empty()) {
+            solve(s, ind + 1, ans, temp + "." + t, count + 1);
+        } else {
+            solve(s, ind + 1, ans, t, count + 1);
+        }
+    }
+}
+
+    vector<string> generateIp(string &s) {
+        // Your code here
+        if(s.size()<4)
+        return {"-1"};
+        
+        
+        vector<string> ans;
+        solve(s,0,ans,"",0);
+        
+        return ans;
+        
+    }
+
+};
 
