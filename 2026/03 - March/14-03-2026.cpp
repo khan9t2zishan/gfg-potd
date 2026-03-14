34919338350de62454234c7d5d313db1a312669f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-14 22:06:06 +0530

    Update 14-03-2026.cpp

--- 2026/03 - March/14-03-2026.cpp
+++ 2026/03 - March/14-03-2026.cpp
@@ -1,1 +1,44 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
 
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        if (!root) return {};
+        map<int, Node*> mp;
+        queue<pair<Node*, int>> q;
+
+        q.push({root, 0});
+
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+
+            Node* node = it.first;
+            int hd = it.second;
+
+            if (!mp.count(hd)) mp[hd] = node;
+
+            if (node->left) q.push({node->left, hd-1});
+
+            if (node->right) q.push({node->right, hd+1});
+        }
+
+        vector<int> ans;
+        for (auto &it : mp) ans.push_back(it.second->data);
+        return ans;
+    }
+};
