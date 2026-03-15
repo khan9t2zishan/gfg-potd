2064c4e464efc8dfb6fd2dfdbc4d96fa25d2f392
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-15 20:42:35 +0530

    Update 15-03-2026.cpp

--- 2026/03 - March/15-03-2026.cpp
+++ 2026/03 - March/15-03-2026.cpp
@@ -1,1 +1,40 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
 
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<vector<int>> verticalOrder(Node *root) {
+        // code here
+        map<int,vector<int>> mp;
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+        while(!q.empty()){
+            auto [node, lvl] = q.front();
+            q.pop();
+            mp[lvl].push_back(node->data);
+            if(node->left){
+                q.push({node->left, lvl-1});
+            }
+            if(node->right){
+                q.push({node->right, lvl+1});
+            }
+        }
+        vector<vector<int>> res;
+        for(auto &it:mp){
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
