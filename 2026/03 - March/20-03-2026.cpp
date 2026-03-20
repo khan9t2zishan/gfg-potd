b0fa3713d742886b4fe993233e41929bcf924f0c
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-20 21:43:50 +0530

    Update 20-03-2026.cpp

--- 2026/03 - March/20-03-2026.cpp
+++ 2026/03 - March/20-03-2026.cpp
@@ -1,1 +1,40 @@
+/* BST Node
+class Node {
+   public:
+    int data;
+    Node *left;
+    Node *right;
 
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<Node*> findPreSuc(Node* root, int key) {
+        // code here
+        Node*ans1 = NULL;
+        Node*ans2 = NULL;
+        Node*temp = root;
+        
+        while(temp){
+            if(temp->data > key)ans1=temp;
+            if(temp->data <= key)temp = temp->right;
+            else temp=temp->left;
+        }
+        
+        
+        temp=root;
+        while(temp){
+            if(temp->data < key)ans2=temp;
+            if(temp->data < key)temp = temp->right;
+            else temp=temp->left;
+        }
+        
+        return {ans2,ans1};
+    }
+};
