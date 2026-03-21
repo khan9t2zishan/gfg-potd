fdbea05015700bd4807803a5b4c2b6fe9b3096c4
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-03-21 22:06:25 +0530

    Number of BST From Array

--- 2026/03 - March/21-03-2026.cpp
+++ 2026/03 - March/21-03-2026.cpp
@@ -7,7 +7,7 @@ class Solution {
             int m = l+(r-l)/2;
             if(arr[m] == key)return m;
             else if(arr[m]<key)l = m+1;
-            else r = m-1;
+            else r = m-1; 
         }
         return -1;
     }   
