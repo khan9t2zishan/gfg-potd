bea24b552b535a10d0c3b1cdd1f86300d8d920dc
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-03 22:52:44 +0530

    Update 03-04-2026.cpp

--- 2026/04 - April/03-04-2026.cpp
+++ 2026/04 - April/03-04-2026.cpp
@@ -1,1 +1,24 @@
+class Solution {
+  public:
+    vector<int> diagView(vector<vector<int>> mat) {
+        // code here
+        int n=mat.size();
+        vector<int>ans;
+        
+        unordered_map<int, vector<int>>mpp;
 
+        for(int i=0; i<n; i++){
+          for(int j=0; j<n; j++){
+            mpp[i+j].push_back(mat[i][j]);
+          }
+        }
+
+        for(int i=0; i<=n*n; i++){
+            for(int &j:mpp[i]){
+                ans.push_back(j);
+            }
+        }
+
+        return ans;
+    }
+};
