73ce3c54462d7a24cf2dac8899f0c337bf0c8bfe
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-04 21:18:56 +0530

    Update 04-04-2026.cpp

--- 2026/04 - April/04-04-2026.cpp
+++ 2026/04 - April/04-04-2026.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+  public:
+    vector<string> graycode(int n) {
+        // code here
+        const int size = 1 << n;
+        
+        vector<string> result(size, string(n, '0'));
+        
+        for (int i = 1; i < size; i++) {
+            int gray = i ^ (i >> 1);
+            string& s = result[i];
+            
+            for (int g = gray, j = n - 1; g; g >>= 1, j--) {
+                if (g & 1) s[j] = '1';
+            }
+        }
+        
+        return result;
+    }
+};
