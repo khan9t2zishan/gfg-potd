2abff0692ffb070fe2f2d954ba2b5ccf773e70a5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-11 22:22:02 +0530

    Update 11-04-2026.cpp

--- 2026/04 - April/11-04-2026.cpp
+++ 2026/04 - April/11-04-2026.cpp
@@ -3,7 +3,7 @@ class Solution {
     int countIncreasing(vector<int>& arr) {
         // code here.
         int ans=0;
-        for(int i=1, k=1; i<arr.size(); i++){
+        for(int i=1, k=1; i<arr.size(); i++){ 
             if(arr[i]>arr[i-1]){
                 ans+=k++;
             }
