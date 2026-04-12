b6e1c4bf0c931dc09b8b97a27cfa2a7e4008213f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-12 21:16:32 +0530

    Toeplitz matrix

--- 2026/04 - April/12-04-2026.cpp
+++ 2026/04 - April/12-04-2026.cpp
@@ -4,7 +4,7 @@ class Solution {
         // code here
         int m = mat.size();
         int n = mat[0].size();
- 
+  
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 if(mat[i][j]!=mat[i-1][j-1])return false;
