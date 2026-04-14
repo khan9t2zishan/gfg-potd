6475503734e2b3eb61c84af1b8afb920cef245be
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-14 21:33:36 +0530

    Update 14-04-2026.cpp

--- 2026/04 - April/14-04-2026.cpp
+++ 2026/04 - April/14-04-2026.cpp
@@ -3,7 +3,7 @@ class Solution {
     string removeSpaces(string& s) {
         // code here
         string ans = "";
-        for(int i=0; i<s.length(); i++){
+        for(int i=0; i<s.length(); i++){ 
             if(s[i] != ' '){ 
                 ans.push_back(s[i]);
             }
