b680a36b094d918dc85279a5f2a7bfb922e4e4e9
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-14 21:33:54 +0530

    Remove Spaces

--- 2026/04 - April/14-04-2026.cpp
+++ 2026/04 - April/14-04-2026.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     string removeSpaces(string& s) {
         // code here
-        string ans = "";
+        string ans = ""; 
         for(int i=0; i<s.length(); i++){ 
             if(s[i] != ' '){ 
                 ans.push_back(s[i]);
