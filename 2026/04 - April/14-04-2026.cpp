729dcf03a585e0eaea946cc847bbcfafe0a00c87
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-14 21:33:30 +0530

    Update 14-04-2026.cpp

--- 2026/04 - April/14-04-2026.cpp
+++ 2026/04 - April/14-04-2026.cpp
@@ -4,7 +4,7 @@ class Solution {
         // code here
         string ans = "";
         for(int i=0; i<s.length(); i++){
-            if(s[i] != ' '){
+            if(s[i] != ' '){ 
                 ans.push_back(s[i]);
             }
         }
