53260ec488abdb2135fe5a3d3d67112028fe28ad
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-15 18:56:48 +0530

    Update 15-04-2026.cpp

--- 2026/04 - April/15-04-2026.cpp
+++ 2026/04 - April/15-04-2026.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+  public:
+    string URLify(string &s) {
+        // code here
+        string ans;
+        int i=0;
+        while(i<s.size() && s[i]==' '){
+            i++;
+        }
+        int j=s.size()-1;
+        while(i<=j){
+            if(s[i]==' '){
+                ans+="%20";
+            }
+            else{
+                ans+=s[i];
+            }
+            i++;
+        }
+        return ans;
+    }
+};
