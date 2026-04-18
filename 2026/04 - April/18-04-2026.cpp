1c2d85cb19e94548ffc5bc17bc920c66e4a19796
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-18 18:22:33 +0530

    Update 18-04-2026.cpp

--- 2026/04 - April/18-04-2026.cpp
+++ 2026/04 - April/18-04-2026.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+  public:
+    int maxOnes(vector<int>& arr) {
+        // code here
+        stack<int> st;
+        int ans = 0, ctr = 0;
+        for(int i=0; i<arr.size(); i++) {
+            if(arr[i] == 1) {
+                if(st.size()) st.pop();
+                ctr++;
+            } else {
+                st.push(0);
+                ans = max(ans, (int)st.size());
+            }
+        }
+        return ans + ctr; 
+    }
+};
