ebb870894aa0a007297de1db85524c078afbffa2
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-21 21:38:25 +0530

    Two water Jug problem

--- 2026/04 - April/21-04-2026.cpp
+++ 2026/04 - April/21-04-2026.cpp
@@ -4,7 +4,7 @@ class Solution {
     int gcd(int a, int b) {
         while (b != 0) {
             int temp = a % b;
-            a = b;
+            a = b; 
             b = temp; 
         }
         return a;
