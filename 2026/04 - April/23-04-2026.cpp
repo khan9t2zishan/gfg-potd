9a3ecaebb70a199b3e9d442b6cd1b2481796f1c8
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-23 21:39:13 +0530

    Update 23-04-2026.cpp

--- 2026/04 - April/23-04-2026.cpp
+++ 2026/04 - April/23-04-2026.cpp
@@ -1,1 +1,14 @@
+class Solution {
+  public:
+    bool canSplit(vector<int>& arr) {
+        // code here
+        int sum = 0, bsum = 0;
+        for(int &i:arr)sum+=i;
+        for(int &i:arr){
+            bsum+=i;
+            if(sum - 2*bsum == 0)return true;
+        }
+        return false;
+    }
+};
 
