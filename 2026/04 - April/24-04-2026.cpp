153dd7dd7b57a95e61d4bebceff1520b0a03622d
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-24 22:29:01 +0530

    Update 24-04-2026.cpp

--- 2026/04 - April/24-04-2026.cpp
+++ 2026/04 - April/24-04-2026.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+  public:
+    int visibleBuildings(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int maxheight = arr[0];
+        int count = 0;
+        for(int i =0;i<n;i++){
+            if(arr[i]>=maxheight){
+                count ++;
+                maxheight=arr[i];
+            }
+        }
+        return count;
+    }
+};
