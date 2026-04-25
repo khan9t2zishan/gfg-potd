2353096f11fb52ad19117cd6ac51b16258f7096b
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-04-25 21:13:32 +0530

    Update 25-04-2026.cpp

--- 2026/04 - April/25-04-2026.cpp
+++ 2026/04 - April/25-04-2026.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-    vector<int> reducePairs(vector<int>& arr) {
+    vector<int> reducePairs(vector<int>& arr) { 
         // code here
         int n=arr.size();
         stack<int>st;
