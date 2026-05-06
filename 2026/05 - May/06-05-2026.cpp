ef252b602ca1005984112e815d58aab0f2ec7140
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-06 22:27:47 +0530

    Update 06-05-2026.cpp

--- 2026/05 - May/06-05-2026.cpp
+++ 2026/05 - May/06-05-2026.cpp
@@ -1,1 +1,25 @@
+/*
+Definition for Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
 
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    int getSize(Node* root) {
+        // code here
+         if(root==NULL) return 0;
+             
+             int left = getSize(root->left);
+             int right = getSize(root->right);
+             
+             return 1+left+right;
+    }
+};
