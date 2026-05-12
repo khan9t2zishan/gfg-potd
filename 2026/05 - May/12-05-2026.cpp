bfb73f3bf52430cab17e1a897aa3e765e5d7a1c7
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-12 21:55:04 +0530

    Range LCM Queries

--- 2026/05 - May/12-05-2026.cpp
+++ 2026/05 - May/12-05-2026.cpp
@@ -9,7 +9,7 @@ vector<long long> seg;
         return a;
     }
 
-    long long lcm(long long a, long long b) {
+    long long lcm(long long a, long long b) { 
         return (a / gcd(a, b)) * b;
     }
 
