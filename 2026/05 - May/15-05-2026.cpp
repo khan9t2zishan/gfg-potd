37afc59e7119877182d70917d985ee15d3b16af4
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-15 21:39:48 +0530

    Special Keyboard

--- 2026/05 - May/15-05-2026.cpp
+++ 2026/05 - May/15-05-2026.cpp
@@ -7,7 +7,7 @@ class Solution {
 		for (int i = 3; i <= n; i++) {
 			int mp = 2;
 			for (int j = i - 3; j >= 0; j--) {
-				dp[i] = max(dp[i], dp[j]*mp); // Math.max(dp[i], dp[j]*mp) for Java
+				dp[i] = max(dp[i], dp[j]*mp); 
 				mp++;
 			}
 		}
