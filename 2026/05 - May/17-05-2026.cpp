d88113282ec9e4c34afcdd0aaa24d3c013310ac6
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-17 21:29:07 +0530

    Update 17-05-2026.cpp

--- 2026/05 - May/17-05-2026.cpp
+++ 2026/05 - May/17-05-2026.cpp
@@ -1,1 +1,22 @@
+class Solution {
+	public:
+	vector<int> makeBeautiful(vector<int> arr) {
+		// code here
+		int size = arr.size();
+		deque<int> dq;
+		
+		for (int i = 0; i < size; i++) {
+			// Same sign
+			if (dq.empty() || (dq.back() >= 0 && arr[i] >= 0) || (dq.back() < 0 && arr[i] < 0)) {
+				dq.push_back(arr[i]);
+			}
+			// Different sign
+			else {
+				dq.pop_back();
+			}
+		}
+		
+		return vector<int> (dq.begin(), dq.end());
+	}
+};
 
