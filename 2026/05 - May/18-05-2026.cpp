aaf1b4b3f882dc9b7c0b18478b8d37281a893766
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-18 22:51:51 +0530

    Maximum Sum Problem

--- 2026/05 - May/18-05-2026.cpp
+++ 2026/05 - May/18-05-2026.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int maxSum(int n) {
         // code here. 
-        vector<int> dp(n+1, 0);
+        vector<int> dp(n+1, 0); 
         for(int i = 0;i<=n;i++){
             dp[i] = max(i, (dp[i/2] + dp[i/3] + dp[i/4]));
         }
