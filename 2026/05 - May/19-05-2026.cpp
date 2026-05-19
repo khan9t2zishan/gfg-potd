18e11bc63844fdb5d373db7e9072cb65423c9c8b
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-19 21:25:44 +0530

    Update 19-05-2026.cpp

--- 2026/05 - May/19-05-2026.cpp
+++ 2026/05 - May/19-05-2026.cpp
@@ -1,1 +1,39 @@
+class Solution {
+	public:
+	int minSteps(vector<int>& arr, int start, int end) {
+		// code here
+		if (start == end)
+			return 0;
+		
+		vector<int> dist(1000, 1e9);
+		
+		queue<int> q;
+		
+		dist[start] = 0;
+		q.push(start);
+		
+		while (!q.empty()) {
+			
+			int current = q.front();
+			q.pop();
+			
+			for (int num : arr) {
+				
+				int next = (current * num) % 1000;
+				
+				if (dist[current] + 1 < dist[next]) {
+					
+					dist[next] = dist[current] + 1;
+					
+					if (next == end)
+						return dist[next];
+					
+					q.push(next);
+				}
+			}
+		}
+		
+		return - 1;
+	}
+};
 
