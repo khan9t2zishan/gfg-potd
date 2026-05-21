47b93d4c02ff3ca391274da4fcd975eec172a631
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-21 21:36:38 +0530

    Update 21-05-2026.cpp

--- 2026/05 - May/21-05-2026.cpp
+++ 2026/05 - May/21-05-2026.cpp
@@ -1,1 +1,20 @@
+class Solution {
+	public:
+	bool isBitSet(int n) {
+		// code here
+		if (n == 0) {
+			return false;
+		}
+		
+		while (n > 0) {
+			if ((n&1) == 0) {
+				return false;
+			}
+			
+			n>>= 1;
+		}
+		
+		return true;
+	}
+};
 
