9b2a5982bcda1c2ce42246e183cb02c9e7ce73ca
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-26 21:49:24 +0530

    Update 26-05-2026.cpp

--- 2026/05 - May/26-05-2026.cpp
+++ 2026/05 - May/26-05-2026.cpp
@@ -1,1 +1,30 @@
+class Solution {
+	public:
+	int minToggle(vector<int>& arr) {
+		// code here
+		int rightZeros = 0;
+		
+		for (int num : arr) {
+			if (num == 0)
+				rightZeros++;
+		}
+		
+		int ans = rightZeros;
+		
+		int leftOnes = 0;
+		
+		for (int num : arr) {
+			
+			if (num == 0)
+				rightZeros--;
+			
+			else
+				leftOnes++;
+			
+			ans = min(ans, leftOnes + rightZeros);
+		}
+		
+		return ans;
+	}
+};
 
