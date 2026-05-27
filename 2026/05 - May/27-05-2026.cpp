5ca51baa5900ed06afcc5b697299739100e8e219
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-27 19:51:03 +0530

    Update 27-05-2026.cpp

--- 2026/05 - May/27-05-2026.cpp
+++ 2026/05 - May/27-05-2026.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+  public:
+    bool wifiRange(string &s, int x) {
+        // code here
+     int n=s.length();
+        int flg=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1'){
+                if(i-x>flg){
+                    return 0;
+                } 
+                flg=max(flg,i+x+1);
+            }
+        }
+        return flg>=n;
+    }
+};
