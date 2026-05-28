1d3ed1494efe6c6e9d415105457042a0e0e0acb5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-05-28 20:51:54 +0530

    Update 28-05-2026.cpp

--- 2026/05 - May/28-05-2026.cpp
+++ 2026/05 - May/28-05-2026.cpp
@@ -1,1 +1,32 @@
-
+/* Structure of binary tree node
+class Node{
+public:
+    int data;
+    Node* left, right;
+    Node(int item)
+    {
+        data = item;
+        left = nullptr;
+        right = nullptr;
+    }
+}
+*/
+class Solution {
+  public:
+     void dfs(Node* root,int key,map<int,int> &mp){
+      if(!root)return ;
+      mp[key]+=root->data;
+      dfs(root->left,key-1,mp);
+      dfs(root->right,key+1,mp);
+  }
+    vector<int> verticalSum(Node* root) {
+        // code here
+        vector<int> ans;
+        map<int,int> mp;
+        dfs(root,0,mp);
+        for(auto i:mp){
+            ans.push_back(i.second);
+        }
+        return ans; 
+    }
+};
