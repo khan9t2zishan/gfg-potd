50639dd89ba1dfd8418613a4c334b0c7bf6b295a
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-06-07 20:53:44 +0530

    Finding Profession

--- 2026/06 - June/07-06-2026.cpp
+++ 2026/06 - June/07-06-2026.cpp
@@ -1,7 +1,7 @@
 class Solution {
 	public:
 	string profession(int level, int pos) {
-		// code here
+		// code here 
 		bool job = 1; // 1 means engineer
 		level = ceil(log2(pos)); // Take upper value of decimal
 		int var = pow(2, level); // help us to move right or left
