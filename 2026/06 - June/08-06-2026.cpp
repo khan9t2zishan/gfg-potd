d3ab010cc03030f0d9081deaa948c16bf4413c35
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-06-08 16:57:54 +0530

    Update 08-06-2026.cpp

--- 2026/06 - June/08-06-2026.cpp
+++ 2026/06 - June/08-06-2026.cpp
@@ -1,1 +1,24 @@
+/* Structure of a Linked List node
+class Node {
+	public:
+	int data;
+	Node* next;
+	
+	Node(int x) {
+		data = x;
+		next = nullptr;
+	}
+};
+*/
+class Solution {
+	public:
+	Node *compute(Node *head) {
+		// code here
+		if (head->next == NULL)return head;
+		Node * tp = compute(head->next);
+		if (tp->data > head->data)return tp;
+		head->next = tp;
+		return head;
+	}
+};
 
