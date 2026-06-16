900d1ddf194175f99b8d232645c986067c622950
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-06-16 13:41:13 +0530

    Update 16-06-2026.cpp

--- 2026/06 - June/16-06-2026.cpp
+++ 2026/06 - June/16-06-2026.cpp
@@ -1,1 +1,23 @@
+class Solution {
+	public:
+	vector<int> constructList(vector<vector<int>> &queries) {
+		// code here
+		vector<int> ans;
+		int xr = 0;
+		ans.push_back(0);
+		
+		for (auto &q : queries) {
+			if (q[0] == 0)
+				ans.push_back(q[1] ^ xr);
+			else
+				xr ^= q[1];
+		}
+		for (int &e : ans)
+			e ^= xr;
+		
+		sort(ans.begin(), ans.end());
+		
+		return ans;
+	}
+};
 
