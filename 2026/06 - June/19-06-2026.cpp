376179dd2a2b0b39bc56708510ab22d96c9e50a1
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-06-19 21:27:19 +0530

    Update 19-06-2026.cpp

--- 2026/06 - June/19-06-2026.cpp
+++ 2026/06 - June/19-06-2026.cpp
@@ -4,7 +4,7 @@ int n = arr.size();
         pref[0] = arr[0];
 
         for (int i = 1; i < n; i++) {
-            pref[i] = pref[i - 1] + arr[i];
+            pref[i] = pref[i - 1] + arr[i]; 
         }
 
         vector<int> ans(n);
