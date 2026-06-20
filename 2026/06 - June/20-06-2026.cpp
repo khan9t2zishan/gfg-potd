8b26a1230bfaf24667eaedeb3a9ff133d780e18e
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-06-20 20:08:09 +0530

    Update 20-06-2026.cpp

--- 2026/06 - June/20-06-2026.cpp
+++ 2026/06 - June/20-06-2026.cpp
@@ -1,1 +1,30 @@
+class Solution {
+	public:
+	int getLastDigit(string& a, string& b) {
+		// code here
+		if (b == "0") {
+			return 1;
+		}
+		int base = a[a.length() - 1]-'0';
+		
+		if (base == 0) {
+			return 0;
+		}
+		
+		int exp = 0;
+		int blen = b.length();
+		if (blen == 1) {
+			exp = (b[0]-'0')%4;
+		}
+		else {
+			int ltd = (b[blen - 2]-'0')*10 + (b[blen - 1]-'0');
+			exp = ltd%4;
+		}
+		if (exp == 0) {
+			exp = 4;
+		}
+		int ans = pow(base, exp);
+		return ans%10;
+	}
+};
 
