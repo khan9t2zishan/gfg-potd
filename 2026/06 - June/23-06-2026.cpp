8da893c2bb2e25a37a9445543d72afd12c8448a2
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-06-23 21:25:18 +0530

    Update 23-06-2026.cpp

--- 2026/06 - June/23-06-2026.cpp
+++ 2026/06 - June/23-06-2026.cpp
@@ -1,1 +1,25 @@
+class Solution {
+	public:
+	int maxPeopleDefeated(int p) {
+		// Code Here
+		int start = 1;
+		int end = 1000;
+		int ans = 0;
+		while (start <= end) {
+			
+			int mid = start + ((end - start)/2);
+			int sum = (mid*(mid + 1)*((mid*2) + 1))/6;
+			
+			if (sum <= p) {
+				ans = mid;
+				start = mid + 1;
+			}
+			else
+				end = mid - 1;
+			
+		}
+		
+		return ans;
+	}
+};
 
