7ecdffd10cf97452e0a4d15309a7b23db7e2741d
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-06-29 20:42:10 +0530

    Update 29-06-2026.cpp

--- 2026/06 - June/29-06-2026.cpp
+++ 2026/06 - June/29-06-2026.cpp
@@ -1,1 +1,33 @@
+class Solution {
+	public:
+	int maxDotProduct(vector<int>& a, vector<int>& b) {
+		// code here
+		int n = a.size(), m = b.size();
+		
+		const int NEG = -1e9;
+		
+		vector<int> dp(m + 1, NEG);
+		
+		dp[0] = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			
+			vector<int> ndp = dp;
+			
+			for (int j = 1; j <= min(i, m); j++) {
+				
+				ndp[j] = max(ndp[j], dp[j]);
+				
+				if (dp[j - 1] != NEG) {
+					ndp[j] = max(ndp[j],
+					dp[j - 1] + a[i - 1] * b[j - 1]);
+				}
+			}
+			
+			dp = ndp;
+		}
+		
+		return dp[m];
+	}
+};
 
