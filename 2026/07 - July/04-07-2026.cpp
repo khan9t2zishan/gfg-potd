c44f08b83f663239b71f28e05d6adb2c66121345
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-07-04 22:23:01 +0530

    Update 04-07-2026.cpp

--- 2026/07 - July/04-07-2026.cpp
+++ 2026/07 - July/04-07-2026.cpp
@@ -1,1 +1,22 @@
+class Solution {
+	public:
+	int countSubstring(string& s) {
+		// Code Here
+		int count = 0;
+		vector<int>mp1(s.size()*2 + 5, 0), mp2(s.size() *2 + 5);
+		int ans = 0;
+		int m = s.size();
+		mp1[m] = 1;
+		mp2[m] = 1;
+		for (auto i: s) {
+			if (i == '1')count++;
+			else
+				count--;
+			mp1[count + m]++;
+			mp2[count + m] = mp1[count + m] + mp2[m + count - 1];
+			ans += mp2[m + count - 1];
+		}
+		return ans;
+	}
+};
 
