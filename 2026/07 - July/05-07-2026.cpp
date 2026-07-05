82c42dabf1df04de8fb9e72956f2df01ce18039f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-07-05 22:22:37 +0530

    Update 05-07-2026.cpp

--- 2026/07 - July/05-07-2026.cpp
+++ 2026/07 - July/05-07-2026.cpp
@@ -1,1 +1,22 @@
+class Solution {
+  public:
+    int maxCharGap(string &s) {
+        vector<int> firstPos(26, -1);
 
+        int maxGap = -1;
+
+        for (int i = 0; i < s.size(); i++) {
+            int index = s[i] - 'a';
+
+            if (firstPos[index] == -1) {
+                firstPos[index] = i;
+            } else {
+                int gap = i - firstPos[index] - 1;
+
+                maxGap = max(maxGap, gap);
+            }
+        }
+
+        return maxGap;
+    }
+};
