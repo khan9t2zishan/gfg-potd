01fd374c296f44906bef70bed1ee8c9644503e44
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-07-06 22:46:37 +0530

    Update 06-06-2026.cpp

--- 2026/07 - July/06-06-2026.cpp
+++ 2026/07 - July/06-06-2026.cpp
@@ -1,1 +1,50 @@
+class Solution {
+	public:
+	int maxPathSum(vector<int> &a, vector<int> &b) {
+		// Code here
+		int i = 0;
+		int j = 0;
+		
+		int sumA = 0;
+		int sumB = 0;
+		
+		int answer = 0;
+		
+		while (i < a.size() && j < b.size()) {
+			if (a[i] < b[j]) {
+				sumA += a[i];
+				i++;
+			}
+			else if (a[i] > b[j]) {
+				sumB += b[j];
+				j++;
+			}
+			else {
+				answer += max(sumA, sumB);
+				
+				answer += a[i];
+				
+				sumA = 0;
+				sumB = 0;
+				
+				i++;
+				j++;
+			}
+		}
+		
+		while (i < a.size()) {
+			sumA += a[i];
+			i++;
+		}
+		
+		while (j < b.size()) {
+			sumB += b[j];
+			j++;
+		}
+		
+		answer += max(sumA, sumB);
+		
+		return answer;
+	}
+};
 
