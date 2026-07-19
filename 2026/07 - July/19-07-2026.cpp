99f0cdb7eb75057197195a5f5094cfae9008cbb5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-07-19 19:51:20 +0530

    Update 19-07-2026.cpp

--- 2026/07 - July/19-07-2026.cpp
+++ 2026/07 - July/19-07-2026.cpp
@@ -1,1 +1,48 @@
+class Solution {
+	public:
+	vector<bool> processQueries(vector<int> &arr, vector<vector<int>> &queries) {
+		// code here
+		int n = arr.size();
+		
+		vector<int> up(n);
+		
+		vector<int> down(n);
+		
+		up[n - 1] = n - 1;
+		
+		for (int i = n - 2; i >= 0; i--) {
+			if (arr[i] <= arr[i + 1])
+				up[i] = up[i + 1];
+			else
+				up[i] = i;
+		}
+		
+		down[n - 1] = n - 1;
+		
+		for (int i = n - 2; i >= 0; i--) {
+			if (arr[i] >= arr[i + 1])
+				down[i] = down[i + 1];
+			else
+				down[i] = i;
+		}
+		
+		vector<bool> ans;
+		
+		for (auto &q : queries) {
+			int l = q[0];
+			int r = q[1];
+			
+			int peak = up[l];
+			
+			if (peak >= r)
+				ans.push_back(true);
+			
+			else
+				ans.push_back(down[peak] >= r);
+		}
+		
+		return ans;
+		
+	}
+};
 
