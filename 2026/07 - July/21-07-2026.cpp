b5434e7fe37e152f525b017f28d99bda1789003e
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-07-21 20:15:54 +0530

    Update 21-07-2026.cpp

--- 2026/07 - July/21-07-2026.cpp
+++ 2026/07 - July/21-07-2026.cpp
@@ -1,1 +1,35 @@
+class Solution {
+	public:
+	int maxIndexDifference(string &s) {
+		// code here
+		vector<int> best(26, -1);
+		
+		int ans = -1;
+		
+		for (int i = (int)s.size() - 1; i >= 0; i--) {
+			
+			int c = s[i] - 'a';
+			
+			int reach;
+			
+			if (c == 25) {
+				reach = i;
+			}
+			else if (best[c + 1] != -1) {
+				reach = best[c + 1];
+			}
+			else {
+				reach = i;
+			}
+			
+			best[c] = max(best[c], reach);
+			
+			if (c == 0) {
+				ans = max(ans, reach - i);
+			}
+		}
+		
+		return ans;
+	}
+};
 
