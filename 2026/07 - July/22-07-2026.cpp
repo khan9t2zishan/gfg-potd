83c22d1c397c15cbe90396574894de38a0326fcb
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-07-22 13:16:09 +0530

    Update 22-07-2026.cpp

--- 2026/07 - July/22-07-2026.cpp
+++ 2026/07 - July/22-07-2026.cpp
@@ -1,1 +1,22 @@
+class Solution {
+  public:
+    int minDeletions(vector<int>& arr) {
+        // code here
+        vector<int> lis;
 
+        for (int num : arr) {
+
+            if (lis.empty() || num > lis.back()) {
+                lis.push_back(num);
+            } else {
+
+                auto it = lower_bound(lis.begin(), lis.end(), num);
+
+                *it = num;
+            }
+        }
+
+        return arr.size() - lis.size();
+
+    }
+};
