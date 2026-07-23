dcd2ce755b31fcb30f2ad1ed4cebeff3b251e3d9
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-07-23 20:35:55 +0530

    Update 23-07-2026.cpp

--- 2026/07 - July/23-07-2026.cpp
+++ 2026/07 - July/23-07-2026.cpp
@@ -1,1 +1,29 @@
+/*
+class Node
+{
+    int data;
+    Node* left;
+    Node* right;
 
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+   void solve(Node* root, vector<int> &ans) {
+        if (root == NULL) return;
+        ans.push_back(root->data);
+        solve(root->left, ans);
+        solve(root->right, ans);
+    }
+
+    vector<int> preOrder(Node* root) {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
