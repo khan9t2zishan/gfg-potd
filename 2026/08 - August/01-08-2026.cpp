918b96f15b6a0783b611c64cff06c319af45dec1
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-08-01 22:27:39 +0530

    Update 01-08-2026.cpp

--- 2026/08 - August/01-08-2026.cpp
+++ 2026/08 - August/01-08-2026.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+  public:
+    int findMax(int n, vector<int>& a, vector<int>& b, vector<int>& k) {
+        // code here
+        vector<int>v(n,0);
+        int m = a.size();
+        int sum = 0;
+        int ans = 0;
+        
+        for(int i=0;i<m;i++){
+            v[a[i]]+=k[i];
+            if(b[i]+1<n)v[b[i]+1]-=k[i];
+        }
+        
+        
+        for(int i=0;i<n;i++){
+            sum+=v[i];
+            ans = max(ans,sum);
+        }
+        
+        
+        return ans;
+    }
+};
