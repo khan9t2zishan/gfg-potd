3bd704702c8d9e0a4e340f6b9178c0d8bfbe20f6
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-08-02 22:05:59 +0530

    Update 02-08-2026.cpp

--- 2026/08 - August/02-08-2026.cpp
+++ 2026/08 - August/02-08-2026.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+  public:
+    int count(int n, int m) {
+        // code here
+        vector<int>curr(m+1 , 0) , next(m+1 , 0);
+        for(int i = 1 ; i<=m ; i++)curr[i]=1;
+        for(int i =1 ; i<n ; i++){
+            for(int i = 1 ; i<=m ; i++)next[i] = 0;
+            for(int j = 1 ; j<=m ; j++){
+                next[j] += curr[j]; 
+                for(int k = j*2 ; k<=m ; k+=j){
+                    next[k]+= curr[j];
+                    next[j]+= curr[k];
+                }
+            }
+            swap(curr , next);
+        }
+        return accumulate(curr.begin() , curr.end() , 0);
+    }
+};
