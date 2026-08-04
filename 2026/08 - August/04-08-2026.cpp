0ca8d6a1eef97886eb61645ad8a58c95ed614445
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-08-04 22:45:46 +0530

    Update 04-08-2026.cpp

--- 2026/08 - August/04-08-2026.cpp
+++ 2026/08 - August/04-08-2026.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+  public:
+    int countPairs(vector<int>& arr, int k) {
+        // code here
+        int n=arr.size();
+        sort(arr.begin(), arr.end());
+        
+        int i=0, j=0, cnt=0;
+        
+        for(; j<n; j++){
+            while(arr[j]-arr[i]>=k){
+                i++;
+            }
+            cnt+=(j-i);
+        }
+        
+        return cnt;
+    }
+};
