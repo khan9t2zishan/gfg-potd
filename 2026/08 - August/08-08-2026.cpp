b7a6a6904e6f8c26bc5993fe7f2b6901337f36d2
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-08-08 20:15:45 +0530

    Min Edge Movements to Connect a Graph

--- 2026/08 - August/08-08-2026.cpp
+++ 2026/08 - August/08-08-2026.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
      void DFS(int u, vector<vector<int>>& adj, vector<bool>& visited) 
     {
-        visited[u]=true;
+        visited[u]=true; 
         for(int v:adj[u]) 
         {
             if(!visited[v]) 
