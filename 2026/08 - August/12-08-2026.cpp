308fe33b23c1688e78014e8c680b0669084c2a68
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-08-12 22:55:04 +0530

    Update 12-08-2026.cpp

--- 2026/08 - August/12-08-2026.cpp
+++ 2026/08 - August/12-08-2026.cpp
@@ -1,1 +1,64 @@
+class Solution {
+  public:
+      vector<int> findWays(vector<vector<int>>& grid) {
+        int n = grid.size();
+        const long long MOD = 1000000007LL;
 
+        vector<long long> ways(n, 0);
+
+        vector<int> best(n, -1);
+
+        ways[0] = 1;
+
+        best[0] = grid[0][0];
+
+        for (int i = 0; i < n; ++i) {
+
+            for (int j = 0; j < n; ++j) {
+
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                long long currentWays = 0;
+                int currentBest = -1;
+
+                if (j > 0 &&
+                    (grid[i][j - 1] == 1 || grid[i][j - 1] == 3) &&
+                    best[j - 1] != -1) {
+
+                    currentWays = (currentWays + ways[j - 1]) % MOD;
+
+                    currentBest = max(
+                        currentBest,
+                        best[j - 1] + grid[i][j]
+                    );
+                }
+
+                if (i > 0 &&
+                    (grid[i - 1][j] == 2 || grid[i - 1][j] == 3) &&
+                    best[j] != -1) {
+
+                    currentWays = (currentWays + ways[j]) % MOD;
+
+                    currentBest = max(
+                        currentBest,
+                        best[j] + grid[i][j]
+                    );
+                }
+
+                ways[j] = currentWays;
+                best[j] = currentBest;
+            }
+        }
+
+        if (best[n - 1] == -1) {
+            return {0, 0};
+        }
+
+        return {
+            (int)ways[n - 1],
+            best[n - 1]
+        };
+    }
+};
