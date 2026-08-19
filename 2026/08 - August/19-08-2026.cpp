15320b3d81ff8dc2f7700e37ae9d4f6407643566
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-08-19 23:12:14 +0530

    Update 19-08-2026.cpp

--- 2026/08 - August/19-08-2026.cpp
+++ 2026/08 - August/19-08-2026.cpp
@@ -1,1 +1,30 @@
+class Solution {
+  public:
+        long long countLessEqual(vector <int> &arr, int x){
+            int n = arr.size();
+            long long count = 0;
 
+            for (int i = 0; i < n-2; i++){
+                int left = i+ 1;
+                int right = n- 1;
+
+               while(left < right){
+                   long long sum = (long long)arr[i] + arr[left] + arr[right];
+
+                   if(sum <= x){
+                       count += right-left;
+                       left++;
+                   }else{
+                       right--;
+                   }
+               }
+            }
+            return count;
+        }
+        long long countTriplets(vector<int> &arr, int l, int r) {
+              // code here
+              sort(arr.begin(), arr.end());
+
+              return countLessEqual(arr,r) - countLessEqual(arr, l-1);
+    }
+};
