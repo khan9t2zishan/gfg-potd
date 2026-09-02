3d982c15406c973f970c2e86523de59d68089f50
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-09-02 19:38:30 +0530

    Marks from Ranks

--- 2026/08 - August/30-08-2026.cpp
+++ 2026/08 - August/30-08-2026.cpp
@@ -1,1 +1,25 @@
+class Solution {
+  public:
+    vector<int> getMarks(vector<int> &l, vector<int> &r, vector<int> &rank) {
+        // code here
+        vector<int> temp;
 
+        for(int i=0;i<l.size();i++){
+            int low=l[i];
+            int high=r[i];
+
+            for(int j=low;j<=high;j++){
+                temp.push_back(j);
+            }
+        }
+
+        vector<int> ans;
+        for(int i=0;i<rank.size();i++){
+            int pos=rank[i]-1;
+
+            ans.push_back(temp[pos]);
+        }
+
+        return ans;
+    }
+};
