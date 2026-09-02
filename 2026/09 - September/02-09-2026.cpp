2273ac91775693890da1b19c53ee5882038536ee
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-09-02 19:37:35 +0530

    Unoccupied Computers

--- 2026/09 - September/02-09-2026.cpp
+++ 2026/09 - September/02-09-2026.cpp
@@ -1,1 +1,34 @@
+class Solution {
+  public:
+    int solve(int n, string s) {
+        // code here
+        unordered_map<char, int> mp;
+		int size = 0, count = 0;
+		for (char& c : s) {
+			// if a customer is not present and rejected
+			if (mp.count(c) != 0 && mp[c] != -1) {
+				// exit the customer i.e mp[c] = 0;
+				mp[c]--;
+				size--;
+			}
+			else {
+				// rejected customer so move on
+				if (mp[c] == -1)
+					continue;
+
+				// if the cafe has space add the customer
+				if (size < n) {
+					mp[c]++;
+					size++;
+				}
+				else {
+					// reject the customer
+					mp[c] = -1;
+					count++;
+				}
+			}
+		}
+		return count;
+    }
+};
 
