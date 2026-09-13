e7255a39e440af677f0d46818f8aa4669fb0dc88
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2026-09-13 23:55:01 +0530

    Party in Town

--- 2026/09 - September/13-09-2026.cpp
+++ 2026/09 - September/13-09-2026.cpp
@@ -1,27 +1,32 @@
 class Solution {
-public:
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        const int n=img1.size();
-        bitset<30> B1[n], B2[n];
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++){
-                B1[i][j]=img1[i][j];
-                B2[i][j]=img2[i][j];
-            }
-        
-        int ans=0;
-        for(int i=-n+1; i<n; i++){// up/down
-            for(int j=-n+1; j<n; j++){ // left/right
-                int cnt=0;
-                for(int k=0; k<n; k++){// row
-                    int k1=k+i;
-                    if (k1<0 || k1>=n) continue;
-                    auto sB=(j<0)?B1[k1]<<(-j):B1[k1]>>j;
-                    cnt+=(sB & B2[k]).count();
-                }
-                ans=max(ans, cnt);
-            }
-        }
-        return ans;
+  public:
+    int partyHouse(vector<vector<int>> &adj) {
+              int n = adj.size();
+              if (n == 1) return 0;
+
+              auto bfs = [&](int start) {
+                  vector<int> dist(n, -1);
+                  queue<int> q;
+                  q.push(start);
+                  dist[start] = 0;
+                  int far = start;
+                  while (!q.empty()) {
+                      int u = q.front(); q.pop();
+                      for (int v : adj[u]) {
+                          int idx = v - 1;
+                          if (dist[idx] == -1) {
+                              dist[idx] = dist[u] + 1;
+                              q.push(idx);
+                              if (dist[idx] > dist[far]) far = idx;
+                          }
+                      }
+                  }
+                  return make_pair(far, dist[far]);
+              };
+
+              auto [u, _] = bfs(0);
+              auto [v, diameter] = bfs(u);
+
+              return (diameter + 1) / 2;
     }
 };
